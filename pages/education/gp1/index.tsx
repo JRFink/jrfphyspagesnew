2c520b2501db91f9e97cf8577c65ad49bc948856
Jeff F <jeff@jrfink.net>
2023-08-05 23:58:54 -0400

    c

--- pages/education/gp1/index.tsx
+++ pages/education/gp1/index.tsx
@@ -11,7 +11,7 @@ const Gp1 = () => {
                 General Physics 1
             </h1>
             <p>
-                Eventually you will be able to take gen phys 1 here. Have a look at our <a href="/pdf/gp1syl.pdf" download>sample syllabus</a>.
+                Eventually you will be able to take gen phys 1 here. Have a look at our <Link href="/pdf/gp1syl.pdf" className={style.sampleLink} download>sample syllabus</Link>.
             </p>
             <br></br>
             <br></br>
