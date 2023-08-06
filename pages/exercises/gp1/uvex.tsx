49f3078307200a423b05eca53db5b4803a3ac697
Jeff F <jeff@jrfink.net>
2023-08-06 11:04:40 -0400

    c

--- pages/exercises/gp1/uvex.tsx
+++ pages/exercises/gp1/uvex.tsx
@@ -9,10 +9,16 @@ const UvEx = () => {
             Exercises
         </h2>
         <ol>
-            <li>Explain a unit in your own words</li>
+            <li>What are some things you know about physics?</li>
             <br></br>
-            <li>Can units change?</li>
+            <li>How do we start doing physics?</li>
+            <ol type="a">
+              <li>How do we do it qualitatively?</li>
+              <li>How do we do it quantitatively?</li>
+            </ol>
         </ol>
+        <h3>Frames</h3>
+        <p>3. Draw a 2 dimensional cartesian plane</p>
     </div>
   );
 }
