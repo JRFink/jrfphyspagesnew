e17ac10ff20917adaac083b7e2503e88d22b2411
Jeff F <jeff@jrfink.net>
2023-08-06 11:09:01 -0400

    n

--- pages/exercises/gp1/uvex.tsx
+++ pages/exercises/gp1/uvex.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Link from 'next/link';
  
 const UvEx = () => {
   const [count, setCount] = useState(0)
  
   return (
-    <div>
+    <>
         <h2>
             Exercises
         </h2>
@@ -17,9 +18,25 @@ const UvEx = () => {
               <li>How do we do it quantitatively?</li>
             </ol>
         </ol>
-        <h3>Frames</h3>
-        <p>3. Draw a 2 dimensional cartesian plane</p>
-    </div>
+        <h3>
+          Frames
+        </h3>
+        <p>
+          3. Draw a 2 dimensional cartesian plane
+        </p>
+        <p>
+          4. Draw a 3 dimensional cartesian plane
+        </p>
+        <h3>
+          Units
+        </h3>
+        <p>
+          5. What is a standard?
+        </p>
+        <p>
+          6. Familiarize yourself with <Link href="https://www.nist.gov">www.nist.gov</Link>
+        </p>
+    </>
   );
 }
 
