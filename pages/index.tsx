001fb19108eae4183c4783122f0c42f16c0f7e4d
Jeff Fink <fink.jeffr@gmail.com>
2023-08-11 22:53:37 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ const indexPage = () => {
   return (
     <>
       <p className={style.centerText}>
-        Physics Education & Research
+        Hello, & welcome to jrfink.net! This site focuses on physics education and research. Take a look around and explore. Above you will see four tabs, which will take you to the education, research, sign up and sign-in tabs. There is also a search feature which we are implementing. 
+
+You come to this site to learn about physics. 
       </p>   
     </>
   );
