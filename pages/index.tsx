87f337fa6a4bdbe3951f4cb5a6386f0131cfa0c4
Jeff F <jeff@jrfink.net>
2023-08-11 23:21:39 -0400

    e

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,14 @@ const indexPage = () => {
   return (
     <>
       <p className={style.centerText}>
-        Hello, and welcome to jrfink.net! This site focuses on physics education and research. Take a look around and explore. Above you will see four tabs, which will take you to the education, research, sign up and sign-in tabs. There is also a search feature which we are implementing. 
+        Hello, and welcome to jrfink.net! This site focuses on physics education and research. <br></br> <br></br>
+        Take a look around and explore. Above you will see four tabs, which will take you to the education, 
+        research, sign up and sign-in tabs. There is also a search feature which we are implementing. <br></br> <br></br>
 
-You come to this site to learn about physics. 
+        You come to this site to learn about physics. <br></br>
+        Eventually you will be able to take gen phys 1, a full-on implementation of a general physics 1 class. <br></br><br></br> 
+
+        Also, you are able to learn about the current research topics within physics.
       </p>   
     </>
   );
