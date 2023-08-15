2d5bfc4c3271c8ee14230e919cf3cd3ebf8a109d
Jeff F <jeff@jrfink.net>
2023-08-15 09:52:15 -0400

    u

--- pages/exercises/gp1/nlex.tsx
+++ pages/exercises/gp1/nlex.tsx
@@ -7,8 +7,8 @@ const NlEx = () => {
   return (
     <div>
         <Exerciseheading />
-        <ol>
-            <li>Write down the 3 laws in your own words</li>
+        <ol className={style.exerciseOl}>
+            <li>Write down the  laws in your own words</li>
             <br></br>
             <li>Write down Newton&#39;s second law for gravity as a general second order ODE. Use p&#39;s and q&#39;s.</li>
             <br></br>
