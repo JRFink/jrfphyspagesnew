787769ab5e7512279a8d7725ffc2a1bfeaeee1f5
Jeff F <jeff@jrfink.net>
2023-08-15 01:19:56 -0400

    l

--- pages/education/gp1/index.tsx
+++ pages/education/gp1/index.tsx
@@ -20,12 +20,12 @@ const Gp1 = () => {
                 Description
             </h2>
             <p>
-                This class is an introduction to general physics. It will cover the following topics: 
+                An introduction to general physics. It will cover the following topics: 
                 <br></br>
                 <br></br>
                 &nbsp; &nbsp; kinematics, forces &#40;newton&#39;s laws&#41;, work, energy, rotational motion, taylor expansion, gravity, waves, fluid motion, and intro thermodynamics. 
                 <br></br><br></br>
-                The class consists of ~ 20 modules, which will consist of lecture videos, homework assignments, quizzes, exams, an extra credit paper, and a final exam.
+                The class consists of ~ 20 modules, which consist of lecture videos, homework assignments, quizzes, exams, an extra credit paper, and a final exam.
                 I hope you enjoy it, and please give me your <a href="mailto:jeff@jrfink.net">feedback</a> as you go through it.
                 <br></br>
                 <br></br>
