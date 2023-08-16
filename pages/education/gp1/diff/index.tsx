eefa6dacccaa34c6b7aa55b21e7c5f68c6ba7fd2
Jeff Fink <fink.jeffr@gmail.com>
2023-08-16 11:25:15 -0400

    Update index.tsx

--- pages/education/gp1/diff/index.tsx
+++ pages/education/gp1/diff/index.tsx
@@ -12,6 +12,8 @@ const Osc = () => {
             <h1 className={style.centerText}>
                 Differential equations 
             </h1>
+            <p> 
+                Differential equations isn&#39;t something you normally learn in gen phys 1, but learning a few pieces of it will drastically help you understand Newton&#39s laws more effectively. More specifically, will help you understand the second law and how to solve it in different cases. In some cases it is easy to solve; in other cases it isn&#39;t and that&#39;s why you have to result to other means. 
             <ModoverviewDiff />
                 <h2 className={style.h2SectionColor} id="types">
                     1. Types
