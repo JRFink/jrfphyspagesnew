632c61f478f8759dea88ba704bbe4c459652d98e
Jeff F <jeff@jrfink.net>
2023-08-16 07:28:44 -0400

    e

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -30,6 +30,8 @@ const Footer = () => {
                 <br></br>
                 <Link href="/legal/access" className={style.linkFooter}>Accessibility</Link><br></br>
                 <br></br>    
+                <Link href="/legal/lang" className={style.linkFooter}>Language</Link><br></br>
+                <br></br>  
                 <div className={style.footerGenCountry}>
                     <p>
                         Country
---
+++ pages/legal/lang/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Lang = () => {
+    return (
+        <>
+            <h1 className={style.centerText}>
+                Language
+            </h1>
+        </>
+    );
+}
+
+export default Lang;
