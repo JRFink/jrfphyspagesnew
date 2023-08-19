a3cb6d7c93be36f933255310ba173024ec7562a1
Jeff F <jeff@jrfink.net>
2023-08-19 00:58:03 -0400

    h

--- pages/exam/test/gp1/2/index.tsx
+++ pages/exam/test/gp1/2/index.tsx
@@ -7,6 +7,26 @@ const OnedmTest = () => {
             <Link href="/education/gp1" className={style.backtoLink}>Gen phys 1</Link>
             <br></br>
             <h1 className={style.centerText}>Test 2</h1>
+            <p>
+                Test 2 will cover the following topics:
+                <ol>
+                    <li>
+                        Simple differential equations
+                    </li>
+                    <li>
+                        Newton&#39;s first, second, and third law
+                    </li>
+                    <li>
+                        Applications of Newton&#39;s laws
+                    </li>
+                    <li>
+                        Work and kinetic energy
+                    </li>
+                    <li>
+                        Potential energy and energy conservation
+                    </li>
+                </ol>
+            </p>
         </>
     );
 }
