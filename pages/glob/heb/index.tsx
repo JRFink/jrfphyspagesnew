002dcf271fae01af1d06ba13cb7b3b2ceb96f07d
Jeff F <jeff@jrfink.net>
2023-08-20 15:00:50 -0400

    d

--- pages/glob/heb/index.tsx
+++ pages/glob/heb/index.tsx
@@ -1,13 +1,13 @@
 import style from '@/styles/general.module.css';
 
-const schqiptareShtëpi = () =>  { 
+const עבריתבית = () =>  { 
     return (
         <>
            <h1 className={style.centerText}>
-                fizikë
+                 פיזיקה           
            </h1>        
         </>
     );
 } 
 
-export default schqiptareShtëpi;
+export default עבריתבית;
