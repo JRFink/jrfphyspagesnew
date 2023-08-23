3d9d2d0a4285d565b7651e12f1af50371464fd7b
Jeff F <jeff@jrfink.net>
2023-08-23 08:43:39 -0400

    x

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,30 +1,10 @@
-import style from '@/styles/general.module.css';
+import LoginForm from '@/pages/components/loginForm';
 
 const Login = () => {
 
     return (
         <>
-            <h1 className={style.centerText}>
-                Login
-            </h1>
-            <br></br>
-            <p className={style.centerText}>
-                just testing rn
-            </p>
-            <br></br>
-            <div className={style.formDiv}>
-                <form className={style.formClass}> 
-                    <div className={style.inputDiv}>
-                        <input name="email" id="email" type="text" className={style.inputBox} placeholder="email" required></input>
-                        <label className={style.labelBox}><b>Email: </b></label>
-                    </div>
-                    <div className={style.inputDiv}>
-                        <input name="password" id="password" type="password" className={style.inputBox} placeholder="&#x2A;&#x2A;&#x2A;&#x2A;nottooeasy&#x2A;&#x2A;&#x2A;&#x2A;" required></input>
-                        <label className={style.labelBox}><b>Password: </b></label>
-                    </div>
-                    <button type="submit" className={style.greenButton}>Login</button>
-                </form>
-            </div>
+            <LoginForm />
         </>
     );
 }
---
+++ pages/components/loginForm.tsx
@@ -0,0 +1,37 @@
+import style from '@/styles/general.module.css';
+
+const LoginForm = () => {
+    
+    return (
+        <>
+            <h1 className={style.centerText}>
+                Login
+            </h1>
+            <br></br>
+            <p className={style.centerText}>
+                just testing rn
+            </p>
+            <br></br>
+            <div className={style.formDiv}>
+                <form className={style.formClass}> 
+                    <h2 className={style.formHeader}> 
+                        Login 
+                    </h2> 
+                    <div className={style.inputDiv}>
+                        <input name="email" id="email" type="text" className={style.inputBox} placeholder="email" required></input>
+                        <label className={style.labelBox}><b>Email: </b></label>
+                    </div>
+                    <div className={style.inputDiv}>
+                        <input name="password" id="password" type="password" className={style.inputBox} placeholder="&#x2A;&#x2A;&#x2A;&#x2A;nottooeasy&#x2A;&#x2A;&#x2A;&#x2A;" required></input>
+                        <label className={style.labelBox}><b>Password: </b></label>
+                    </div>
+                    <div className={style.centerText}>
+                        <button type="submit" className={style.greenButton}>Login</button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default LoginForm;
