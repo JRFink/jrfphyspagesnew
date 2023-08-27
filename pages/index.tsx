3ec65dd6b6329c358657b2b3a7211fc983d52186
Jeff Fink <fink.jeffr@gmail.com>
2023-08-27 01:42:54 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,6 +60,12 @@ const indexPage = () => {
       <p> 
           If you&#39;re a member of the general public, you can find useful things all over the site. For example, let&#39;s say you in computer programming and a client wamts to experiment with quantum computing. You don&#39;t have to be a quantum expert, but you probably want to know some of the math. You can head on over to our <Link href="/education/qm1/op">math background</Link> section to get an overview of the math used in quantum. 
       </p>
+      <h2> 
+           We&#39;d love your feedback
+      </h2> 
+      <p>
+           We&#39;d love your <Link href="/contact">feedback</Link> on the site. How do you like it? As a thank you, we are giving the first 20 people who give feedback on the site 2 months free once the subscription feature launches. 
+      </p>
       <br></br>
       <p>
         Take a look around and explore. Above you will see a few tabs, which will take you to the education, 
