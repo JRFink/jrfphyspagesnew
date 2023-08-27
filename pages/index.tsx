d8fe0a3a59bade4f39ef492996d39c96fddaf800
Jeff Fink <fink.jeffr@gmail.com>
2023-08-27 01:09:49 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,6 +33,12 @@ const indexPage = () => {
       <p> 
           If you&#39;re in high school and taking physics, head over to our <Link href="/education/gp1">Gen phys 1</Link> page to learn about the topics you&#39;ll cover in class. We&#39;ll cover displacement, the kinematic equations, conservation of energy, and all the essential topics you&#39;ll need to know.
       </p>
+      <h2> 
+          If you&#39;re in college&#46;&#46;&#46; 
+      </h2>
+      <p>
+          If you&#39;re in college and taking gen phys 1, either calc or algebra based, head on over to our <Link href="/education/gp1">Gen phys 1</Link> page. We&#39;ll derive the kinematic equations both from algebra and through calc. We&#39;ll also give you a bit of a differential equations crash-course, so you can understand the different contexts of newton&#39;s second law.  
+      </p>
       <br></br>
       <p>
         Take a look around and explore. Above you will see a few tabs, which will take you to the education, 
