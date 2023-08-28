0d42fd5cb1161b6a36354d17fae6f731919a1789
Jeff Fink <jeff@jrfink.net>
2023-08-27 23:18:07 -0400

    Create index.tsx

---
+++ pages/glob/ind/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css'; 
+  
+ const Indonesia = () =>  {  
+     return ( 
+         <> 
+            <h1 className={style.centerText}> 
+                 fisika 
+            </h1>         
+         </> 
+     ); 
+ }  
+  
+ export default Indonesia;
