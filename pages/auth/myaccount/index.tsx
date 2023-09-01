12559dc55a81aab063325fc9da694b8bbfa20714
Jeff F <jeff@jrfink.net>
2023-08-31 23:45:38 -0400

    s

--- pages/auth/myaccount/index.tsx
+++ pages/auth/myaccount/index.tsx
@@ -6,6 +6,19 @@ const Myaccount = () => {
             <h1 className={style.centerText}>
                 My account
             </h1>
+            <br />
+            <p>
+                Your first name is:
+            </p>
+            <p>
+                Your last name is:
+            </p>
+            <p>
+                Your email is:
+            </p>
+            <p>
+                Your phone number is:
+            </p>
         </>
     );
 }
