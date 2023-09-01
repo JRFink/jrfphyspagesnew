c0fc52dc3b0c7cf9e39baa005520762cb8aaa1a9
Jeff F <jeff@jrfink.net>
2023-09-01 00:33:41 -0400

    s

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -6,26 +6,28 @@ import profsvg from '@/public/images/profsvg.svg';
 const Navbar = () => {
     return (
         <>
+            <Link href="/auth/myaccount" className={style.moveRight}>
+                <Image
+                    src={profsvg}
+                    style={{
+                        width: '4%',
+                        borderRadius: "20px",
+                        height: 'auto'
+                    }}
+                    sizes="100vw"
+                    alt="A colorful drawing of a cell" 
+                    className={style.avatar}
+                    
+                />
+            </Link>
             <div className={style.flexContainer}>
+            
                 <Link href="/" className={style.styleNav}>Home</Link>
                 <Link href="/education" className={style.styleNav}>Education</Link>
                 <Link href="/research" className={style.styleNav}>Research</Link>  
                 <input type="text" className={style.searchBox} placeholder="Search topic"></input>
                 <Link href="/auth/login" className={style.styleNav}>Login</Link>
                 <Link href="/auth/signup" className={style.styleNav}>Sign Up</Link>
-                {/* <Link href="/auth/myaccount" className={style.styleNav}> */}
-                <Image
-                    src={profsvg}
-                    style={{
-                        width: '7%',
-                        borderRadius: "20px",
-                        height: 'auto'
-                    }}
-                    sizes="100vw"
-                    alt="A colorful drawing of a cell" 
-                />
-                {/* </Link> */}
-       
             </div>
             <br></br>
         </>
