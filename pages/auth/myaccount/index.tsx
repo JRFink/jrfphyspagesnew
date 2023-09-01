a0ae4919aa746da579986bc8944ad2e71ee90282
Jeff F <jeff@jrfink.net>
2023-08-31 23:44:02 -0400

    df

---
+++ pages/auth/myaccount/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Myaccount = () => {
+    return (
+        <>
+            <h1 className={style.centerText}>
+                My account
+            </h1>
+        </>
+    );
+}
+
+export default Myaccount;
--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                 <input type="text" className={style.searchBox} placeholder="Search topic"></input>
                 <Link href="/auth/login" className={style.styleNav}>Login</Link>
                 <Link href="/auth/signup" className={style.styleNav}>Sign Up</Link>
-                <Link href="auth/myaccount" className={style.styleNav}>My account</Link>
+                <Link href="/auth/myaccount" className={style.styleNav}>My account</Link>
        
             </div>
             <br></br>
