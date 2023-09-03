e2a737b53533d1c901e5ca2c8ad8932c844788b4
Jeff Fink <jeff@jrfink.net>
2023-09-02 22:18:39 -0400

    Update index.tsx

--- pages/glob/turk/index.tsx
+++ pages/glob/turk/index.tsx
@@ -1,1 +1,13 @@
+import style from '@/styles/general.module.css';
 
+const eviTürk = () =>  { 
+    return (
+        <>
+           <h1 className={style.centerText}>
+                fizik
+           </h1>        
+        </>
+    );
+} 
+
+export default eviTürk;
