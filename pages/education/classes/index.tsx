633437a91e984ed7a643287ecc75f410b27c7d6b
Jeff Fink <fink.jeffr@gmail.com>
2023-09-04 14:40:13 -0600

    Update index.tsx

--- pages/education/classes/index.tsx
+++ pages/education/classes/index.tsx
@@ -6,8 +6,21 @@ const Classes = () => {
        <h1 className={style.centerText}>
            Classes
        </h1>
-    </>
-  ); 
+<Link href="/education/hop1" className={style.generalLink}>History of Physics 1</Link><br></br>
+                <Link href="/education/hop2" className={style.generalLink}>History of Physics 2</Link><br></br>
+                <Link href="/education/gp1" className={style.generalLink}>General Physics 1</Link><br></br>
+                <Link href="/education/gp2" className={style.generalLink}>General Physics 2</Link><br></br>
+                <Link href="/education/imp" className={style.generalLink}>Intro to Modern Physics</Link><br></br>
+                <Link href="/education/cm1" className={style.generalLink}>Classical Mechanics 1</Link><br></br>
+                <Link href="/education/cm2" className={style.generalLink}>Classical Mechanics 2</Link><br></br>
+                <Link href="/education/em1" className={style.generalLink}>Electricity & Magnetism 1</Link><br></br>
+                <Link href="/education/em2" className={style.generalLink}>Electricity & Magnetism 2</Link><br></br>
+                <Link href="/education/therm" className={style.generalLink}>Thermodynamics</Link><br></br>
+                <Link href="/education/qm1" className={style.generalLink}>Quantum Mechanics 1</Link><br></br>
+                <Link href="/education/qm2" className={style.generalLink}>Quantum Mechanics 2</Link><br></br> 
+</>
+
+ ); 
 }
 
 export default Classes;
