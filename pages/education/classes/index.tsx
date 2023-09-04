c5b6cd2507036317625a98d33fa4e8603e22cabd
Jeff Fink <fink.jeffr@gmail.com>
2023-09-04 15:03:12 -0600

    Update index.tsx

--- pages/education/classes/index.tsx
+++ pages/education/classes/index.tsx
@@ -6,8 +6,7 @@ const Classes = () => {
        <h1 className={style.centerText}>
            Classes
        </h1>
-<Link href="/education/hop1" className={style.generalLink}>History of Physics 1</Link><br></br>
-                <Link href="/education/hop2" className={style.generalLink}>History of Physics 2</Link><br></br>
+
                 <Link href="/education/gp1" className={style.generalLink}>General Physics 1</Link><br></br>
                 
 </>
