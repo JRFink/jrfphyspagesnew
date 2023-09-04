136475894c13c245abda4d7a2059595e64dd4079
Jeff Fink <fink.jeffr@gmail.com>
2023-09-04 14:42:34 -0600

    Update index.tsx

--- pages/education/topics/index.tsx
+++ pages/education/topics/index.tsx
@@ -6,6 +6,15 @@ const Topics = () => {
            <h1 className={style.centerText}>
              Topics
            </h1>
+<Link href="/education/gp2" className={style.generalLink}>General Physics 2</Link><br></br>
+                <Link href="/education/imp" className={style.generalLink}>Intro to Modern Physics</Link><br></br>
+                <Link href="/education/cm1" className={style.generalLink}>Classical Mechanics 1</Link><br></br>
+                <Link href="/education/cm2" className={style.generalLink}>Classical Mechanics 2</Link><br></br>
+                <Link href="/education/em1" className={style.generalLink}>Electricity & Magnetism 1</Link><br></br>
+                <Link href="/education/em2" className={style.generalLink}>Electricity & Magnetism 2</Link><br></br>
+                <Link href="/education/therm" className={style.generalLink}>Thermodynamics</Link><br></br>
+                <Link href="/education/qm1" className={style.generalLink}>Quantum Mechanics 1</Link><br></br>
+                <Link href="/education/qm2" className={style.generalLink}>Quantum Mechanics 2</Link><br></br> 
        </>
     );
 }
