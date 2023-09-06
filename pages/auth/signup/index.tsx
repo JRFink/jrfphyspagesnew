58c5e5bb05ddf65b24113dc081f5c35d2b64af9f
Jeff Fink <fink.jeffr@gmail.com>
2023-09-05 20:13:14 -0600

    Update index.tsx

--- pages/auth/signup/index.tsx
+++ pages/auth/signup/index.tsx
@@ -106,6 +106,7 @@ just keep a free account right now.
             <br /> 
             <br />
             We are going to offer two different types of accounts &#45; free and subscribed. The free accounts will have access to some parts of the site &#45; some videos, articles, research topics, etc. The subscribed accounts will have access to everything &#45; all videos, articles, topics, etc. We&#39;re thinking &#36;5 &#47; month. < br /> 
+<br />
 Let us know what you think <Link href="/contact">here</Link>. 
             </p> 
             <br />
