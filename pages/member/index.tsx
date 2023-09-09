54bf37866cfe77e341ae4885f56f419c7eea9e30
Jeff Fink <jeff@jrfink.net>
2023-09-08 22:49:35 -0400

    Create index.tsx

---
+++ pages/member/index.tsx
@@ -0,0 +1,15 @@
+import style from '@/styles/general.module.css';
+
+const Member = () => { 
+ return ( 
+ <>
+  <h1 className={style.centerText}>
+    Members 
+  </h1> 
+   <p className={style.centerText}>
+   Welcome to the members page! 
+   </p>
+ </>
+  );
+
+} 
