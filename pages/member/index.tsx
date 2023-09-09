135549d0748b8d689a38ff107fc514e068d405a0
Jeff F <jeff@jrfink.net>
2023-09-09 00:54:05 -0400

    f

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -16,34 +16,51 @@ return (
 
 } 
 
+const handleSubmit = (e: any, setNumberCorrect: any) => {
+  e.preventDefault();
+  console.log('e is ' + e.target.input.value);
+  const inputData = e.target.input.value;
+  if (inputData == '8523164') {
+    setNumberCorrect(true);
+  }
+  else {
+    setNumberCorrect(false);
+  }
+
+}
+
 const Member = () => { 
 
-const [numberCorrect, setNumberCorrect] = useState(true);
+const [numberCorrect, setNumberCorrect] = useState(false);
  
  return ( 
- <>
-  <h1 className={style.centerText}>
-    Members 
-  </h1> 
-   <p className={style.centerText}>
-   Welcome to the members page!
-<br /> 
-Enter your access number here... 
-   </p>
-   { 
-    if (numberCorrect) { 
-        return (
-         <>
-            <ConstantPosVid />
-         </>
-        );
+  <>
+    <h1 className={style.centerText}>
+      Members 
+    </h1> 
+    <p className={style.centerText}>
+      Welcome to the members page!
+    <br /> 
+    <br />
+      Enter your access number... 
+    </p>
+    <br />
+    <div className={style.centerText}>
+      <form onSubmit={(e) => handleSubmit(e, setNumberCorrect)}>
+        <input type='text' id='input'></input>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+    { 
+      numberCorrect ? (
+        <ConstantPosVid />
+      ) : (
+        <>
+        </>
+      )
     }
-    return <Sorry />
-   }
- 
- </>
+  </>
   );
-
 } 
 
 export default Member;
