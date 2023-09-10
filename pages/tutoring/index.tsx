febfbe4604368c92c8806c9ae5bfe1887c85fc9c
Jeff F <jeff@jrfink.net>
2023-09-09 22:54:37 -0400

    r

--- pages/tutoring/index.tsx
+++ pages/tutoring/index.tsx
@@ -1,13 +1,16 @@
+import React from 'react';
+import style from '@/styles/general.module.css';
+
 const Tutoring = () => { 
 
   return ( 
      <>
-     <h1 className={style.centerText}>
-       Tutoring
+      <h1 className={style.centerText}>
+        Tutoring
       </h1>
-      <p>
-       Schedule a tutoring appointment here...
-      </p>
+        <p>
+        Schedule a tutoring appointment here...
+        </p>
      </>
   );
 } 
