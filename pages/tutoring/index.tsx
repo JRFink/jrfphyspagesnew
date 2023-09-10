1d12373e5bdf4a8088e657d96304d31882607503
Jeff Fink <jeff@jrfink.net>
2023-09-09 22:29:11 -0400

    Update index.tsx

--- pages/tutoring/index.tsx
+++ pages/tutoring/index.tsx
@@ -2,11 +2,14 @@ const Tutoring = () => {
 
   return ( 
      <>
-     <h1 className={style.centerText>
+     <h1 className={style.centerText}>
        Tutoring
       </h1>
+      <p>
+       Schedule a tutoring appointment here...
+      </p>
      </>
-  )
+  );
 } 
 
 export default Tutoring;
