f2af44fb762c1bc4b0852acb195edc1b345eb952
Jeff Fink <fink.jeffr@gmail.com>
2023-09-15 08:52:04 -0600

    Update index.tsx

--- pages/education/gp1/1dm/index.tsx
+++ pages/education/gp1/1dm/index.tsx
@@ -13,7 +13,7 @@ import Helpful from '@/pages/components/helpful';
 const Onedm = () => {
     return (
         <>
-            <Link href="/education/gp1" className={style.backtoLink}>Gen phys 1</Link><br></br>
+            <Link href="/education/gp1#intro" className={style.backtoLink}>Gen phys 1</Link><br></br>
             <br></br>
             <h1 className={style.centerText}>
                 Motion in 1 Dimension
