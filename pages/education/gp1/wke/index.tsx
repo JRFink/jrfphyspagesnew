26225bacf702d20b85e128fed4cb23fef9d4c84a
Jeff Fink <fink.jeffr@gmail.com>
2023-09-15 09:02:34 -0600

    Update index.tsx

--- pages/education/gp1/wke/index.tsx
+++ pages/education/gp1/wke/index.tsx
@@ -9,7 +9,7 @@ import Helpful from '@/pages/components/helpful';
 const Wke = () => {
     return (
         <>
-            <Link href="/education/gp1" className={style.backtoLink}>Gen phys 1</Link><br></br>
+            <Link href="/education/gp1#intro" className={style.backtoLink}>Gen phys 1</Link><br></br>
             <br></br>
             <h1 className={style.centerText}>
                 Work and Kinetic Energy
