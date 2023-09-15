6c9ca009efd46f23e5d9f6189eb50f8494f4502e
Jeff Fink <fink.jeffr@gmail.com>
2023-09-15 15:42:42 -0600

    Update nlex.tsx

--- pages/exercises/gp1/nlex.tsx
+++ pages/exercises/gp1/nlex.tsx
@@ -9,6 +9,9 @@ const NlEx = () => {
         <Exerciseheading />
         <ol>
 <li> 
+Write down the laws in your own words. 
+</li> 
+<li> 
      Newton&#39;s first law says that an object in motion will remain in motion unless acted on by another; it also states that an object at rest will remain at rest unless acted on by another. Think of a simple situation where Newton&#39;s first law doesn&#39;t hold. 
 </li>
         </ol>
