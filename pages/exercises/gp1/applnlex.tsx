b4016c665c5d2b768f049ce57a69891c696b603c
Jeff Fink <jeff@jrfink.net>
2023-09-15 17:33:15 -0400

    Create applnlex.tsx

---
+++ pages/exercises/gp1/applnlex.tsx
@@ -0,0 +1,20 @@
+import style from '@/styles/general.module.css'; 
+ import { useState } from 'react'; 
+ import Exerciseheading from '@/pages/components/exerciseheading'; 
+  
+ const ApplnlEx = () => { 
+  
+   return ( 
+     <div> 
+         <Exerciseheading />    
+ <ol className={style.exerciseOl}> 
+             <li>Write down the  laws in your own words</li> 
+             <br></br> 
+             <li>Write down Newton&#39;s second law for gravity as a general second order ODE. Use p&#39;s and q&#39;s.</li> 
+             <br></br> 
+             <li>Solve it.</li> 
+   </ol>
+   </div>
+ ); 
+} 
+ export default ApplnlEx; 
