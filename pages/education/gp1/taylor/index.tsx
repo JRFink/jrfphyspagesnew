e8e5f6c172c93b92f6468c34b8bbffb969ce003c
Jeff Fink <fink.jeffr@gmail.com>
2023-09-15 09:00:20 -0600

    Update index.tsx

--- pages/education/gp1/taylor/index.tsx
+++ pages/education/gp1/taylor/index.tsx
@@ -12,7 +12,7 @@ import Helpful from '@/pages/components/helpful';
 const Taylor = () => {
     return (
         <>
-            <Link href="/education/gp1" className={style.backtoLink}>Gen phys 1</Link><br></br>
+            <Link href="/education/gp1#intro" className={style.backtoLink}>Gen phys 1</Link><br></br>
             <br></br>
             <h1 className={style.centerText}>
                 Taylor Expansion
