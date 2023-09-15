cd6e2088ab62843fb98a965b5e26e0b1db8c9f50
Jeff Fink <fink.jeffr@gmail.com>
2023-09-15 15:47:57 -0600

    Update wkeex.tsx

--- pages/exercises/gp1/wkeex.tsx
+++ pages/exercises/gp1/wkeex.tsx
@@ -6,6 +6,14 @@ const WkeEx = () => {
   return (
     <>
       <Exerciseheading />
+<ol> 
+<li> 
+Start standing on the ground. Carry a briefcase up a ladder, across a roof, down a ladder, then walk backwards to where you started. How much work did you do on the briefcase? 
+</li> 
+<li> 
+Start standing on the ground. Carry a briefcase up a ladder, across a roof, then down a ladder. How much work did you do on the briefcase? 
+</li>  
+</ol>
     </>
   );
 }
