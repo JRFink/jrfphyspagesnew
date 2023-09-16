b58ec106f4b5efef02d675cad77bed157f1fb9fe
Jeff Fink <fink.jeffr@gmail.com>
2023-09-15 18:07:15 -0600

    Update puex.tsx

--- pages/exercises/gp1/puex.tsx
+++ pages/exercises/gp1/puex.tsx
@@ -9,7 +9,7 @@ const PuEx = () => {
       <Exerciseheading />
       <ol> 
 <li> 
-Consider the gravitational system of the earth and the sun. The sun and the earth are sometimes closer to one another and sometimes further, and their relative speeds change based upon this. Where does the energy come from when the sun and earth pick-up speeds.  
+Consider the gravitational system of the earth and the sun. The sun and the earth are sometimes closer to one another and sometimes further, and their relative speeds change based upon this. Where does the energy come from when the sun and earth pick-up speed?  
 </li> 
 </ol> 
     </>
