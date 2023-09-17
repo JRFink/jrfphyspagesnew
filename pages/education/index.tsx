933d7c92127d2471fd4a115d0d73887031a3054c
Jeff Fink <fink.jeffr@gmail.com>
2023-09-17 01:34:23 -0400

    Update index.tsx

--- pages/education/index.tsx
+++ pages/education/index.tsx
@@ -53,7 +53,7 @@ Your state education requirements...
 </ul>
 <br /> 
 <p> 
-Also here are the <a href="https://apstudents.collegeboard.org/courses/ap-physics-c-mechanics">college board</a> requirements.
+Also here are the college board<a href="https://apstudents.collegeboard.org/courses/ap-physics-c-mechanics"> calc based</a> requirements.
 </p>
 <br />         
  </div>
