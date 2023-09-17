e822bb8965b56063db252e65050775e09f8b5f03
Jeff Fink <fink.jeffr@gmail.com>
2023-09-16 22:39:22 -0400

    Update index.tsx

--- pages/education/index.tsx
+++ pages/education/index.tsx
@@ -18,7 +18,10 @@ const Education = () => {
                 <br />   
                 <Link href="/education/vid" className={style.generalLink}>Videos</Link>
                 <br />   
-  
+ <p>
+Part of what we want to do is teach you things that you would be required to learn in class. For example, if you are taking physics in Colorado, what are the requirements? We list some of the state requirements below. 
+
+</p>
                 <p> 
 Your state education requirements...
 </p> 
