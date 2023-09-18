7aff7027897ef31b28fe2eab3691b0b2f024490d
Jeff Fink <fink.jeffr@gmail.com>
2023-09-18 11:08:51 -0400

    Update index.tsx

--- pages/education/gp1/flaw/index.tsx
+++ pages/education/gp1/flaw/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import style from '@/styles/general.module.css';
 import { Modoverview1st } from '@/pages/components/modoverview';
+import LearnGoal from '@/pages/components/learngoal';
 import Summary from '@/pages/components/summary';
 import FlawEx from '@/pages/exercises/gp1/flawex';
 import OnedmQz from '@/pages/exam/quiz/gp1/1dmqz';
@@ -25,6 +26,8 @@ const Flaw = () => {
             heat capacities of an ideal gas 
             adiabatic processes for an ideal gas  */}
             <Modoverview1st />
+<br /> 
+<LearnGoal />
 <h2> 
 Introduction 
 </h2>
