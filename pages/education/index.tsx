b632f2f5530ee9d1ae11f2f43d21a0725202eea2
Jeff F <jeff@jrfink.net>
2023-09-18 10:01:59 -0400

    a

--- pages/education/index.tsx
+++ pages/education/index.tsx
@@ -9,7 +9,6 @@ const Education = () => {
                 Education
             </h1>
             <br></br>
-            <br></br>
             <div className={style.flexContainer}>
                 
                 <Link href="/education/classes" className={style.generalLink}>Classes</Link>
@@ -19,48 +18,69 @@ const Education = () => {
                 <Link href="/education/vid" className={style.generalLink}>Videos</Link>
                 <br />   
             </div>
- <p>
-Part of what we want to do is teach you things that you would be required to learn in class. For example, if you are taking physics in Colorado, what are the requirements? We list some of the state requirements below. 
-</p>
-<p> Mind you that these are high school requirements; college requirements can vary. 
-</p>
-                <p> 
-Your state education requirements...
-</p> 
-<ul>
-<li>
-<a href="https://www.cde.ca.gov/re/cc/">California</a></li>
-<br />
-<li>
-<a href="http://www.cde.state.co.us/standardsandinstruction">Colorado</a></li>
-<br />
-<li>
-<a href="https://cpalms.org">Florida</a> - though you have to subscribe </li>
-<br />
-<li> 
-<a href="https://www.isbe.net/Pages/Standards-Courses.aspx">Illinois</a></li>
-<br />
-<li><a href="https://www.doe.mass.edu/frameworks/default.html">Massachusetts</a></li>
-<br />
-<li><a href="https://www.nysed.gov/curriculum-instruction">New York</a></li>
-<br />
-<li> 
-<a href="https://education.ohio.gov/Topics/Learning-in-Ohio/OLS-Graphic-Sections/Learning-Standards">Ohio</a></li>
-<br />
-<li><a href="https://tea.texas.gov/academics/curriculum-standards">Texas</a></li>
-<br /> 
-<li>
-<a href="https://ospi.k12.wa.us/student-success/learning-standards-instructional-materials">Washington</a></li>
-</ul>
-<br /> 
-<p> 
-Also here are the College Board &#174; <a href="https://apstudents.collegeboard.org/courses/ap-physics-1-algebra-based">algebra based</a> requirements and the <a href="https://apstudents.collegeboard.org/courses/ap-physics-c-mechanics">calc based</a> requirements.
-</p>
-<p>
-<a href="https://www.ipho-new.org/documentations/">Physics olympiad </a>problems
-</p>
+            <br />
+            <br />
+            <p>
+                Part of what we want to do is teach you things that you would learn in class. 
+                For example, if you are taking physics in Colorado, what would you learn? We list some of the state requirements below. 
+            </p>
+            <p> 
+                Mind you that these are high school requirements; college requirements vary. 
+            </p>
+            <p> 
+                Your state education requirements...
+            </p> 
+            <br />
+            <ul>
+                <li>
+                    <a href="https://www.cde.ca.gov/re/cc/">California</a>
+                </li>
+                <br />
+                <li>
+                    <a href="http://www.cde.state.co.us/standardsandinstruction">Colorado</a>
+                </li>
+                <br />
+                <li>
+                    <a href="https://cpalms.org">Florida</a> 
+                </li>
+                <br />
+                <li> 
+                    <a href="https://www.isbe.net/Pages/Standards-Courses.aspx">Illinois</a>
+                </li>
+                <br />
+                <li>
+                    <a href="https://www.doe.mass.edu/frameworks/default.html">Massachusetts</a>
+                </li>
+                <br />
+                <li>
+                    <a href="https://www.nysed.gov/curriculum-instruction">New York</a>
+                </li>
+                <br />
+                <li> 
+                    <a href="https://education.ohio.gov/Topics/Learning-in-Ohio/OLS-Graphic-Sections/Learning-Standards">Ohio</a>
+                </li>
+                <br />
+                <li>
+                    <a href="https://tea.texas.gov/academics/curriculum-standards">Texas</a>
+                </li>
+                <br /> 
+                <li>
+                    <a href="https://ospi.k12.wa.us/student-success/learning-standards-instructional-materials">Washington</a>
+                </li>
+            </ul>
+            <br /> 
+            <p> 
+                Here are the College Board &#174; <a href="https://apstudents.collegeboard.org/courses/ap-physics-1-algebra-based">algebra based </a>
+                and <a href="https://apstudents.collegeboard.org/courses/ap-physics-c-mechanics">calc based</a> requirements.
+            </p>
+            <p>
+                <a href="https://www.ibo.org/programmes/diploma-programme/curriculum/sciences/physics/">IB physics</a>
+            </p>
+            <p>
+                <a href="https://www.ipho-new.org/documentations/">Physics olympiad </a>problems
+            </p>
 
-<br />         
+            <br />         
 
             <h2 className={style.edHeaderSpace}>
                 See what students are saying
