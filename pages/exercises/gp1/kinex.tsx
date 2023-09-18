77411178abb168843d523cf9f1b6d680218612a2
Jeff Fink <fink.jeffr@gmail.com>
2023-09-18 10:58:00 -0400

    Update kinex.tsx

--- pages/exercises/gp1/kinex.tsx
+++ pages/exercises/gp1/kinex.tsx
@@ -6,6 +6,11 @@ const KinEx = () => {
     return (
       <>
         <Exerciseheading />
+<ul>
+<li> 
+         Look up the average distance between gas molecules at room temperature. How many moles are in a 1 cubic meter box? 
+</li>
+</ul>
       </>
     );
   }
