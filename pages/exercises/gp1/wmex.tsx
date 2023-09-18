0dda108e0d0dab5aab974913afe78056f8bf9426
Jeff Fink <fink.jeffr@gmail.com>
2023-09-18 11:01:43 -0400

    Update wmex.tsx

--- pages/exercises/gp1/wmex.tsx
+++ pages/exercises/gp1/wmex.tsx
@@ -8,6 +8,9 @@ const WmEx = () => {
         <Exerciseheading />
         <ol>
             <li>How would you describe something that goes up and down?</li>
+<li> 
+        Consider a wave on a string. Consider a different wave on a string. How could each wave be similiar? How could each wave be different? Try to characterize it mathematically. 
+</li> 
         </ol>
     </div>
   );
