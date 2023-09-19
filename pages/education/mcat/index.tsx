3f8e5800134b6fbde75e1e5f51e1f1609d9bfa9d
Jeff Fink <jeff@jrfink.net>
2023-09-19 17:28:50 -0400

    Create index.tsx

---
+++ pages/education/mcat/index.tsx
@@ -0,0 +1,15 @@
+const Mcat = () => { 
+  return ( 
+     <> 
+        <h1 className={style.centerText}>
+             MCAT &#174; phys prep
+        </h1>
+     <p>   
+     Find some prep material 
+       <a href="https://www.mcat-prep.com/mcat-physics-equations-sheet/"> here</a>.
+     </p>
+     </>
+  );  
+}
+
+export default Mcat; 
