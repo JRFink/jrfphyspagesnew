f5d502198bf4405e5e20f9cfb612c606e705bd7a
Jeff Fink <jeff@jrfink.net>
2023-09-18 23:02:46 -0400

    Create index.tsx

---
+++ pages/education/ap/apcelec/index.tsx
@@ -0,0 +1,15 @@
+const ApCelec = () => {  
+   return (  
+      <>  
+        <h1 className={style.centerText}> 
+           AP Physics C &#174; - e and m  
+        </h1> 
+ <p>  
+   Find the curriculum  
+   <a href="https://apstudents.collegeboard.org/courses/ap-physics-c-electricity-and-magnetism">here</a>. 
+ </p>  
+      </> 
+ ); 
+ }  
+  
+ export default ApCelec;
