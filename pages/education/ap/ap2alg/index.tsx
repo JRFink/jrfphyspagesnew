02af62121a0b9f08310d2efa99608e42c2d4781d
Jeff Fink <jeff@jrfink.net>
2023-09-18 22:55:30 -0400

    Create index.tsx

---
+++ pages/education/ap/ap2alg/index.tsx
@@ -0,0 +1,15 @@
+const Ap2alg = () => {  
+   return (  
+      <>  
+        <h1 className={style.centerText}> 
+           AP Physics 2 &#174; - algebra  
+        </h1> 
+ <p>  
+   Find the curriculum  
+   <a href="https://apstudents.collegeboard.org/courses/ap-physics-1-algebra-based">here</a>. 
+ </p>  
+      </> 
+ ); 
+ }  
+  
+ export default Ap2alg;
