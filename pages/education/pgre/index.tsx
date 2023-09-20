94add05dc98d9b1267423df3d0c572b453c6f2e2
Jeff Fink <fink.jeffr@gmail.com>
2023-09-19 23:35:35 -0400

    Update index.tsx

--- pages/education/pgre/index.tsx
+++ pages/education/pgre/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import style from '@/styles/general.module.css'; 
   
  const Pgre = () => {  
@@ -10,8 +11,8 @@ import style from '@/styles/general.module.css';
       This page will help you prepare for the <Link href="/pdf/fact-sheet-physics.pdf">physics gre</Link>. 
  </p> 
       <p>    
-      Find some prep material  
-        <a href=""> here</a>. 
+      Find some prep material here...  
+        
       </p> 
       </> 
    );   
