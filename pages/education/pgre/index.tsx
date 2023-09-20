4902dde48142bc213cc8d625c25e70de3f2a8897
Jeff Fink <jeff@jrfink.net>
2023-09-19 22:31:37 -0400

    Create index.tsx

---
+++ pages/education/pgre/index.tsx
@@ -0,0 +1,20 @@
+import style from '@/styles/general.module.css'; 
+  
+ const Pgre = () => {  
+   return (  
+      <>  
+         <h1 className={style.centerText}> 
+              Pgre &#174; phys prep 
+         </h1> 
+ <p> 
+      This page will help you prepare for the physics section of the <a href="https://students-residents.aamc.org/about-mcat-exam/about-mcat-exam">MCAT</a>. 
+ </p> 
+      <p>    
+      Find some prep material  
+        <a href=""> here</a>. 
+      </p> 
+      </> 
+   );   
+ } 
+  
+ export default Pgre; 
