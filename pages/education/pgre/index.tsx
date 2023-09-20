b54474a8ea909f58f3d113fd20d69966065300ff
Jeff Fink <fink.jeffr@gmail.com>
2023-09-20 17:00:33 -0400

    Update index.tsx

--- pages/education/pgre/index.tsx
+++ pages/education/pgre/index.tsx
@@ -11,7 +11,18 @@ import style from '@/styles/general.module.css';
       This page will help you prepare for the <Link href="/pdf/fact-sheet-physics.pdf">physics gre</Link>. 
  </p> 
       <p>    
-      Find some prep material here...  
+      The test covers the following topics... 
+<ul>
+  <li>Classical mechanics</li>
+  <li>Electromagnetism</li>
+  <li>Optics and wave</li>
+  <li>Thermodynamics and stat mech</li>
+  <li>Quantum mechanics</li>
+  <li>Atomic phys</li>
+  <li>Special relativity</li>
+  <li>Laboratory methods</li>
+  <li>Special topics</li>
+</ul> 
         
       </p> 
       </> 
