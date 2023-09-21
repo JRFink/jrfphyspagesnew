01d3d4f69e70982f4e2acdbfe05c26a9942e5565
Jeff Fink <fink.jeffr@gmail.com>
2023-09-21 16:42:21 -0400

    Update index.tsx

--- pages/education/pgre/atom/index.tsx
+++ pages/education/pgre/atom/index.tsx
@@ -6,6 +6,13 @@ const Atom = () => {
          <h1 className={style.centerText}>
             Atomic physics
          </h1>
+         <p>
+            The atomic physics section will test you on: 
+  <ol>
+    <li>Bohr model of the atom</li>
+    <li>...</li>
+  </ol>
+         </p>
      </>
   );
 }
