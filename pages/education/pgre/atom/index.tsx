9ca44c28a91868d7cc6ffd3ce0ed10d98c781b1e
Jeff Fink <jeff@jrfink.net>
2023-09-21 16:28:56 -0400

    Create index.tsx

---
+++ pages/education/pgre/atom/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Atom = () => { 
+  return ( 
+     <>
+         <h1 className={style.centerText}>
+            Atomic physics
+         </h1>
+     </>
+  );
+}
+
+export default Atom;
