a8ee9f51c94587dfb9143759766559b12dc5a679
Jeff Fink <jeff@jrfink.net>
2023-09-21 16:28:02 -0400

    Create index.tsx

---
+++ pages/education/pgre/quant/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Quant = () => { 
+  return ( 
+     <>
+         <h1 className={style.centerText}>
+           Quantum mechanics
+         </h1>
+     </>
+  );
+}
+
+export default Quant;
