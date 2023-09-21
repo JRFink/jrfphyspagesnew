15ddbf816da3a42d9c69266d19d24c3f5917d601
Jeff Fink <fink.jeffr@gmail.com>
2023-09-21 16:54:56 -0400

    Update index.tsx

--- pages/education/pgre/therm/index.tsx
+++ pages/education/pgre/therm/index.tsx
@@ -6,6 +6,13 @@ const Therm = () => {
          <h1 className={style.centerText}>
            Thermodynamics and stat mech
          </h1>
+         <p>
+The thermo section of the gre will test you on: 
+<ol> 
+ <li>Work, pressure, heat, temperature</li>
+ <li>Cycles</li> 
+ <li>Particle statistics/li>
+ <li>...</li>
      </>
   );
 }
