c7f3d829c8ff04e53cd31aef948d733fbc0e86b3
Jeff Fink <jeff@jrfink.net>
2023-09-21 16:29:37 -0400

    Create index.tsx

---
+++ pages/education/pgre/rel/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Rel = () => { 
+  return ( 
+     <>
+         <h1 className={style.centerText}>
+           Special relativity
+         </h1>
+     </>
+  );
+}
+
+export default Rel;
