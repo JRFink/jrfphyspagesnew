e37cf184f6c5800ac399e894e13626f00221e78e
Jeff Fink <fink.jeffr@gmail.com>
2023-09-21 16:40:34 -0400

    Update index.tsx

--- pages/education/pgre/quant/index.tsx
+++ pages/education/pgre/quant/index.tsx
@@ -6,6 +6,17 @@ const Quant = () => {
          <h1 className={style.centerText}>
            Quantum mechanics
          </h1>
+         <p> 
+            The quantum mechanics section of the gre will test you on: 
+<ol> 
+  <li>Schrodinger equation theory</li>
+  <li>Schrodinger equation for common systems</li>
+  <li>uncertainty principle</li>
+  <li>angular momentum</li>
+  <li>identical particles</li>
+  <li>perturbation theory</li>
+</ol>
+         </p>
      </>
   );
 }
