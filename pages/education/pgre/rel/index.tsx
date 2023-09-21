87d83d2d5cb0b86eb7128dc03b865618fae3ab25
Jeff Fink <fink.jeffr@gmail.com>
2023-09-21 16:51:36 -0400

    Update index.tsx

--- pages/education/pgre/rel/index.tsx
+++ pages/education/pgre/rel/index.tsx
@@ -6,6 +6,14 @@ const Rel = () => {
          <h1 className={style.centerText}>
            Special relativity
          </h1>
+         <p>
+The special relativity section will test you on:
+<ol>
+<li>Postulates of relativity</li>
+<li>Length and time stretching<li> 
+<li>Relative velocity</li>
+</ol>
+         </p>
      </>
   );
 }
