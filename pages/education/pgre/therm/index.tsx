eb658494acdfc5ce176edd982034b29e6a6ed5dd
Jeff Fink <jeff@jrfink.net>
2023-09-21 16:26:54 -0400

    Create index.tsx

---
+++ pages/education/pgre/therm/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Therm = () => { 
+  return ( 
+     <>
+         <h1 className={style.centerText}>
+           Thermodynamics and stat mech
+         </h1>
+     </>
+  );
+}
+
+export default Therm;
