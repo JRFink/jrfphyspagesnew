55754c74d14fec968d451a5084dad89e1a51748b
Jeff Fink <fink.jeffr@gmail.com>
2023-09-21 16:36:20 -0400

    Update index.tsx

--- pages/education/pgre/cm/index.tsx
+++ pages/education/pgre/cm/index.tsx
@@ -6,6 +6,15 @@ const Cm = () => {
          <h1 className={style.centerText}>
            Classical Mechanics
          </h1>
+         <p>
+             The classical mechanics section will test you on your knowledge of: 
+<ol> 
+  <li>the relation between force and acceleration</li>
+  <li>friction</li>
+  <li>lagrangians of common systems</li>
+  <li>hamiltonians of common systems</li>
+</ol>
+         </p>
      </>
   );
 }
