de0ed5a6d26916e471192c4b2488e625abdc720b
Jeff Fink <jeff@jrfink.net>
2023-09-21 16:32:14 -0400

    Create index.tsx

---
+++ pages/education/pgre/spec/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Spc = () => { 
+  return ( 
+     <>
+         <h1 className={style.centerText}>
+           Special topics
+         </h1>
+     </>
+  );
+}
+
+export default Spc;
