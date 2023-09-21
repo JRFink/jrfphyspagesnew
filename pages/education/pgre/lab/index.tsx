2e0b99fa6f03e7a168cefde29a384ae786b0b28a
Jeff Fink <jeff@jrfink.net>
2023-09-21 16:30:49 -0400

    Create index.tsx

---
+++ pages/education/pgre/lab/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Lab = () => { 
+  return ( 
+     <>
+         <h1 className={style.centerText}>
+           Laboratory methods
+         </h1>
+     </>
+  );
+}
+
+export default Lab;
