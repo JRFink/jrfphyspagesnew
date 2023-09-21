aae3c981c05929366d42505c8ec8179e622ef4d8
Jeff Fink <jeff@jrfink.net>
2023-09-21 11:59:31 -0400

    Create index.tsx

---
+++ pages/education/pgre/em/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Em = () => { 
+  return ( 
+     <>
+         <h1 className={style.centerText}>
+           Electromagnetism
+         </h1>
+     </>
+  );
+}
+
+export default Em;
