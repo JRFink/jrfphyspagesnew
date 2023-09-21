1a19610f62f05c89ff01f642e8d0767f607e2172
Jeff Fink <fink.jeffr@gmail.com>
2023-09-21 11:40:51 -0400

    Update index.tsx

--- pages/education/pgre/index.tsx
+++ pages/education/pgre/index.tsx
@@ -11,20 +11,45 @@ import style from '@/styles/general.module.css';
       This page will help you prepare for the <Link href="/pdf/fact-sheet-physics.pdf">physics gre</Link>. 
  </p> 
       <p>    
-      The test covers the following topics... 
-<ul>
-  <li>Classical mechanics</li>
-  <li>Electromagnetism</li>
-  <li>Optics and wave</li>
-  <li>Thermodynamics and stat mech</li>
-  <li>Quantum mechanics</li>
-  <li>Atomic phys</li>
-  <li>Special relativity</li>
-  <li>Laboratory methods</li>
-  <li>Special topics</li>
-</ul> 
+      The test covers the following topics...
+      </p>
+             <br></br> 
+             <p className={style.numberLink}> 
+                 1 
+             </p> 
+             <Link href="/education/pgre/cm" className={style.generalLink}>Classical mechanics</Link><br></br>
+            <br></br> 
+             <p className={style.numberLink}> 
+                 2 
+             </p> 
+             <Link href="/education/pgre/em" className={style.generalLink}>Electromagnetism</Link><br></br>
+            <br></br> 
+             <p className={style.numberLink}> 
+                 3 
+             </p> 
+             <Link href="/education/pgre/op" className={style.generalLink}>Optics amd waves</Link><br></br>
+            <br></br> 
+             <p className={style.numberLink}> 
+                 4 
+             </p> 
+             <Link href="/education/pgre/therm" className={style.generalLink}>Thermodynamics &#38; stat mech</Link><br></br>
+            <br></br> 
+             <p className={style.numberLink}> 
+                 5
+             </p> 
+             <Link href="/education/pgre/quant" className={style.generalLink}>Quantum mechanics</Link><br></br>
+            <br></br> 
+             <p className={style.numberLink}> 
+                 6
+             </p> 
+             <Link href="/education/pgre/atom" className={style.generalLink}>Atomic physics</Link><br></br>
+            <br></br> 
+             <p className={style.numberLink}> 
+                 7 
+             </p> 
+             <Link href="/education/pgre/spc" className={style.generalLink}>Special relativity</Link><br />
         
-      </p> 
+      
       </> 
    );   
  } 
