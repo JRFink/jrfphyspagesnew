159b6f15ede9a26ed8e0fee4355f152ae0a09b0f
Jeff Fink <jeff@jrfink.net>
2023-09-21 16:24:48 -0400

    Create index.tsx

---
+++ pages/education/pgre/op/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Op = () => { 
+  return ( 
+     <>
+         <h1 className={style.centerText}>
+           Optics amd waves
+         </h1>
+     </>
+  );
+}
+
+export default Op;
