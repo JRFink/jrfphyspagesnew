90b9047b4dee698cbd80590176bcc37ba0d07d74
Jeff Fink <jeff@jrfink.net>
2023-09-21 11:58:18 -0400

    Create index.tsx

---
+++ pages/education/pgre/cm/index.tsx
@@ -0,0 +1,13 @@
+import style from '@/styles/general.module.css';
+
+const Cm = () => { 
+  return ( 
+     <>
+         <h1 className={style.centerText}>
+           Classical Mechanics
+         </h1>
+     </>
+  );
+}
+
+export default Cm;
