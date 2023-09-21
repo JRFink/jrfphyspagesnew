92537c1c448034b8ee2a1121c5aee9f7f57e51f5
Jeff Fink <fink.jeffr@gmail.com>
2023-09-21 16:45:57 -0400

    Update index.tsx

--- pages/education/pgre/em/index.tsx
+++ pages/education/pgre/em/index.tsx
@@ -6,6 +6,15 @@ const Em = () => {
          <h1 className={style.centerText}>
            Electromagnetism
          </h1>
+         <p>
+            The E and M section of the gre will test you on:
+ <ol>
+  <li>Electric field</li>
+  <li>Resistance, capacitance, inductance, voltage in a circuit</li>
+  <li>Maxwell&#39;s equations</li>
+  <li>...</li>
+ </ol>
+         </p>
      </>
   );
 }
