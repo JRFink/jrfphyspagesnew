29221bb21eaa134d8fe846f0cc8eb6656bbb27ae
Jeff F <jeff@jrfink.net>
2023-09-21 23:20:52 -0400

    a

--- pages/education/pgre/therm/index.tsx
+++ pages/education/pgre/therm/index.tsx
@@ -3,17 +3,18 @@ import style from '@/styles/general.module.css';
 const Therm = () => { 
   return ( 
      <>
-         <h1 className={style.centerText}>
-           Thermodynamics and stat mech
-         </h1>
-         <p>
-The thermo section of the gre will test you on: 
-<ol> 
- <li>Work, pressure, heat, temperature</li>
- <li>Cycles</li> 
- <li>Particle statistics/li>
- <li>...</li>
-</p>
+        <h1 className={style.centerText}>
+          Thermodynamics and stat mech
+        </h1>
+          <p>
+            The thermo section of the gre will test you on: 
+          <ol> 
+            <li>Work, pressure, heat, temperature</li>
+            <li>Cycles</li> 
+            <li>Particle statistics</li>
+            <li>...</li>
+          </ol>
+         </p>
      </>
   );
 }
