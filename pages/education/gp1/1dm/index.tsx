160f36284387cd4811ea1c85835a804d7ca92ded
Jeff F <jeff@jrfink.net>
2023-09-22 00:20:08 -0400

    a

--- pages/education/gp1/1dm/index.tsx
+++ pages/education/gp1/1dm/index.tsx
@@ -21,25 +21,23 @@ const Onedm = () => {
             </h1>
             <Modoverview1dm />
             <br />
-<LearnGoal />
-<h2>
-Introduction 
-</h2> 
-<p> 
-We begin our formal study of physics with 1 dimensional motion. This is the simplest motion one can study.
-</p>
-
-
-<br />
-<br />
-<h2 className={style.h2SectionColor}> 
-         Domain of 1 dimension  
-</h2>
-<p>
-The domain of one dimensional motion is motion which we can characterize by 1 independent variable. 
-</p>
-<br /> 
-<br />
+            <LearnGoal />
+            <h2>
+            Introduction 
+            </h2> 
+            <p> 
+            We begin our formal study of physics with 1 dimensional motion. This is the simplest motion one can study.
+            </p>
+            <br />
+            <br />
+            <h2 className={style.h2SectionColor}> 
+                    Domain of 1 dimension  
+            </h2>
+            <p>
+            The domain of one dimensional motion is motion which we can characterize by 1 independent variable. 
+            </p>
+            <br /> 
+            <br />
             <h2 className={style.h2SectionColor} id="pos">
                 1. Position and displacement
             </h2>
@@ -80,15 +78,15 @@ The domain of one dimensional motion is motion which we can characterize by 1 in
                  <h4>
                      &nbsp; &nbsp; 2.1.1 Average  
                  </h4>
-<h4>
-&nbsp; &nbsp; 2.1.2 Instantaneous
-</h4>
-            <h2 className={style.h2SectionColor} id="speed">
-                3. Speed
-            </h2>
-<p> 
-Speed... 
-</p> 
+                <h4>
+                &nbsp; &nbsp; 2.1.2 Instantaneous
+                </h4>
+                            <h2 className={style.h2SectionColor} id="speed">
+                                3. Speed
+                            </h2>
+                <p> 
+                Speed... 
+                </p> 
                 <h3 className={style.h3num}> 
                      3.1 
                  </h3> 
@@ -98,52 +96,69 @@ Speed...
                  <h4>
                      &nbsp; &nbsp; 3.1.1 Average  
                  </h4>
-           <h4> 
-&nbsp; &nbsp; 3.1.2 Instantaneous
-</h4>
-            <h2 className={style.h2SectionColor} id="accel">
-                4. Acceleration
-            </h2>
-            <br></br>
-            <AccelVid />
-            <br></br>
-<h3 className={style.h3num}>
-4.1
-</h3>
-            <h3 className={style.h3text}>
-                Average and instantaneous
+                <h4> 
+                    &nbsp; &nbsp; 3.1.2 Instantaneous
+                    </h4>
+                                <h2 className={style.h2SectionColor} id="accel">
+                                    4. Acceleration
+                                </h2>
+                                <br></br>
+                                <AccelVid />
+                                <br></br>
+                    <h3 className={style.h3num}>
+                    4.1
+                    </h3>
+                    <h3 className={style.h3text}>
+                        Average and instantaneous
+                    </h3>
+                        <h4> 
+                            &nbsp; &nbsp; 4.1.1 Average 
+                        </h4> 
+                        <h4> 
+                            &nbsp; &nbsp; 4.1.2 Instantaneous 
+                        </h4>           
+            <h3 className={style.h3num}>
+            4.2
             </h3>
-            <h4> 
-                 &nbsp; &nbsp; 4.1.1 Average 
-            </h4> 
-            <h4> 
-                 &nbsp; &nbsp; 4.1.2 Instantaneous 
-            </h4>           
-<h3 className={style.h3num}>
-4.2
-</h3>
             <h3 className={style.h3text}>
                 Motion with constant a
             </h3>
             <br></br>
             <DeriveKinVid />
             <br></br>
-<p>
-   The four governing equations of motion with constant <b>a</b> are 
-<ul>
-   <li className={style.highlightText}> x = x_0 + v t</li>
-   <li className={style.highlightText}> v = v_0 + a t</li>
-   <li className={style.highlightText}>x = x_0 + v_0 t + 1/2 a t^2 </li>
-   <li className={style.highlightText}> v = v_0 ^ 2 + 2 a &#916; r</li>
-</ul>     
-</p>
-<br />
-            <h3 className={style.h3num}>
-4.3
-</h3>
+            <br />
+    <p>
+        The four governing equations of motion with constant <b>a</b> are 
+    </p>
+   <br />
+   <div className={style.equationBox}>
+        <div className={style.equation}>x = x_0 + v t</div>
+        
+        <div className={style.equationNumber}>&#40;1&#41;</div>
+    </div>
+   <br />
+   <div className={style.equationBox}>
+        <div className={style.equation}>v = v_0 + a t</div>
+        <div className={style.equationNumber}>&#40;2&#41;</div>
+   </div>
+   <br />
+   <div className={style.equationBox}>
+        <div className={style.equation}>x = x_0 + v_0 t + 1/2 a t^2</div>
+        <div className={style.equationNumber}>&#40;3&#41;</div>
+   </div>
+   <br />
+   <div className={style.equationBox}>
+        <div className={style.equation}>v = v_0 ^ 2 + 2 a &#916; r</div>
+        <div className={style.equationNumber}>&#40;4&#41;</div>
+   </div>
+    <br />
+    <h3 className={style.h3num}>
+        4.3
+    </h3>
             <h3 className={style.h3text}>
                 Motion with non-constant a
             </h3>
+            <br />
             <h2 className={style.h2SectionColor} id="calc">
                 5. Calculus examples
             </h2>
