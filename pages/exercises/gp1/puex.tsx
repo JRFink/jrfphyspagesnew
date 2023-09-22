f87ba44bdc3079e218533b749b042a301ad81b7a
Jeff F <jeff@jrfink.net>
2023-09-22 09:45:07 -0400

    w

--- pages/exercises/gp1/puex.tsx
+++ pages/exercises/gp1/puex.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import style from '@/styles/general.module.css';
 import Exerciseheading from '@/pages/components/exerciseheading';
  
 const PuEx = () => {
@@ -7,7 +8,7 @@ const PuEx = () => {
   return (
     <>
       <Exerciseheading />
-      <ol> 
+      <ol className={style.exerciseOl}> 
 <li> 
 Consider the gravitational system of the earth and the sun. The sun and earth are sometimes closer to one another and sometimes further, and their relative speeds change based upon this. Where does the energy come from when the sun and earth pick-up speed?  
 </li> 
--- pages/exercises/gp1/rmex.tsx
+++ pages/exercises/gp1/rmex.tsx
@@ -1,3 +1,4 @@
+import style from '@/styles/general.module.css';
 import Exerciseheading from '@/pages/components/exerciseheading';
 
 const RmEx = () => {
@@ -6,6 +7,10 @@ const RmEx = () => {
     return (
       <>
         <Exerciseheading />
+      
+        <ol className={style.exerciseOl}>
+          <li>A point particle orbits around an axis. What is its kinetic energy?</li>
+        </ol>
       </>
     );
   }
