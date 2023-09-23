5cc56e7287cb983164c1b14db6f53f0d80a254e4
Jeff F <jeff@jrfink.net>
2023-09-22 23:48:05 -0400

    a

--- pages/education/gp1/equat/index.tsx
+++ pages/education/gp1/equat/index.tsx
@@ -9,6 +9,140 @@ const Equat = () => {
             <p>
                 These are the equations you will use in gen phys 1. 
             </p>
+            <br />
+            <h2 className={style.numberLink}>
+                1
+            </h2>
+            <h2 className={style.h3text}>
+                Motion in 1 dimension
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                2
+            </h2>
+            <h2 className={style.h3text}>
+                Motion in 2 and 3 dimensions
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                3
+            </h2>
+            <h2 className={style.h3text}>
+                Newton&#39;s laws
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                4
+            </h2>
+            <h2 className={style.h3text}>
+                Applying Newton&#39;s laws
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                5
+            </h2>
+            <h2 className={style.h3text}>
+                Work and kinetic energy
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                6
+            </h2>
+            <h2 className={style.h3text}>
+                Potential energy and energy conservation
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                7
+            </h2>
+            <h2 className={style.h3text}>
+                Momentum, Impulse, and collisions
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                8
+            </h2>
+            <h2 className={style.h3text}>
+                Rotational motion
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                9
+            </h2>
+            <h2 className={style.h3text}>
+                Dynamics of rotational motion
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                10
+            </h2>
+            <h2 className={style.h3text}>
+                Equilibrium and elasticity
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                11
+            </h2>
+            <h2 className={style.h3text}>
+                Taylor&#39;s theorem
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                12
+            </h2>
+            <h2 className={style.h3text}>
+                Gravity
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                13
+            </h2>
+            <h2 className={style.h3text}>
+                Fluids
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                14
+            </h2>
+            <h2 className={style.h3text}>
+                Oscillations
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                15
+            </h2>
+            <h2 className={style.h3text}>
+                Waves
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                16
+            </h2>
+            <h2 className={style.h3text}>
+                Temperature and heat
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                17
+            </h2>
+            <h2 className={style.h3text}>
+                Gases
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                18
+            </h2>
+            <h2 className={style.h3text}>
+                First law of thermo
+            </h2>
+            <br />
+            <h2 className={style.numberLink}>
+                19
+            </h2>
+            <h2 className={style.h3text}>
+                Second law of thermo
+            </h2>
+            <br />
         </>
     );
 }
