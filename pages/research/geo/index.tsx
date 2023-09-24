9701a661c798f8aa7a49d06338631cb2cdabf9ba
Jeff Fink <jeff@jrfink.net>
2023-09-23 22:43:19 -0400

    Create index.tsx

---
+++ pages/research/geo/index.tsx
@@ -0,0 +1,18 @@
+import style from '@/styles/general.module.css';
+
+const Geo = () => { 
+  return (
+     <> 
+      <h1 className={style.centerText}>
+         Geophysics 
+      </h1>
+      <p> 
+         Geophysics research centers on the structure 
+        of the earth, its interior, crust, heat transfer, 
+        and physical properties. 
+      </p>
+     </>
+  );
+}
+
+export default Geo; 
