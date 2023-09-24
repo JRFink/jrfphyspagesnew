b169d2e18bd4f4b3756f1c7df7ba7bb546ee0dec
Jeff Fink <fink.jeffr@gmail.com>
2023-09-23 22:53:59 -0400

    Update index.tsx

--- pages/consulting/index.tsx
+++ pages/consulting/index.tsx
@@ -1,4 +1,5 @@
 import style from '@/styles/general.module.css';
+
 const Consult = () => { 
   return ( 
       <> 
@@ -7,6 +8,19 @@ const Consult = () => {
         </h1>
  <p>
   Welcome to the consulting page
+</p>
+<p> 
+    As part of the business model we are going to offer consulting services. 
+</p> 
+<p> 
+     Our main areas will be 
+<ul> 
+     <li>Physics education</li>
+     <li>Physics research</li>
+     <li>Higher ed</li>
+     <li>Education policy</li>
+     <li>Government research</li>
+</ul>
 </p>
       </>
   ); 
