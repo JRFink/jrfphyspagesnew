3f5c887be582af506233395ec87ec8c15c21bb12
Jeff Fink <fink.jeffr@gmail.com>
2023-09-24 17:10:56 -0400

    Update index.tsx

--- pages/education/hop2/rel/index.tsx
+++ pages/education/hop2/rel/index.tsx
@@ -9,6 +9,20 @@ const Rel = () => {
         <h1 className={style.centerText}>
             Development of relativity
         </h1>
+            <h2 className={style.numberLink}> 
+                 4.1 
+             </h2> 
+             <Link href="/education/hop2/rel/max" className={style.generalLink}> 
+                 Basis in maxwell&#39;s equations
+             </Link> 
+             <br /> 
+             <h2 className={style.numberLink}> 
+                 4.2 
+             </h2> 
+             <Link href="/education/hop2/rel/ein" className={style.generalLink}> 
+                 Einstein&#39;s major works
+             </Link> 
+             <br />
        </> 
     );
 }
