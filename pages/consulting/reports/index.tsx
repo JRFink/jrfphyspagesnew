2ffa05535ad3e5491b748f3eeb863280372d6301
Jeff F <jeff@jrfink.net>
2023-09-24 00:39:37 -0400

    a

--- pages/consulting/reports/index.tsx
+++ pages/consulting/reports/index.tsx
@@ -1,11 +1,24 @@
+import Link from 'next/link';
 import style from '@/styles/general.module.css';
 
 const Report = () => {
     return (
         <>
+            <br /> 
+            <Link href="/consulting" className={style.backtoLink}>Consulting</Link>
             <h1 className={style.centerText}>
                 Reports
             </h1>
+            <p>
+                Here we will have some reports for free and others for purchase. Examples will include... 
+            </p>
+            <p>
+                <ul>
+                    <li>List of all colleges in the united states</li>
+                    <li>Education policy in argentina</li>
+                    <li>high school graduation rates in georgia</li>
+                </ul>
+            </p>
         </>
     );
 }
