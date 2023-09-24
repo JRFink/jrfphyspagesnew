88ae70639923dcdcc2ae523cdad97b49f27384d1
Jeff Fink <fink.jeffr@gmail.com>
2023-09-24 18:12:00 -0400

    Update index.tsx

--- pages/education/hop2/res/index.tsx
+++ pages/education/hop2/res/index.tsx
@@ -9,6 +9,55 @@ const Res = () => {
         <h1 className={style.centerText}>
             Modern research areas
         </h1>
+
+<h2 className={style.numberLink}>  
+                  7.1  
+              </h2>  
+              <Link href="/education/hop2/20/feyn" className={style.generalLink}>  
+                  Particle 
+              </Link>  
+              <br />  
+              <h2 className={style.numberLink}>  
+                  7.2  
+              </h2>  
+              <Link href="/education/hop2/20/cold" className={style.generalLink}>  
+                  Nuclear 
+              </Link>  
+              <br />  
+              <h2 className={style.numberLink}>  
+                  7.3  
+              </h2>  
+              <Link href="/education/hop2/20/wein" className={style.generalLink}>  
+                  Quantum computing 
+              </Link>  
+              <br />   
+              <h2 className={style.numberLink}>  
+                  7.4  
+              </h2>  
+              <Link href="/education/hop2/20/dirac" className={style.generalLink}>  
+                  
+              </Link>  
+              <br />  
+              <h2 className={style.numberLink}>  
+                  7.5  
+              </h2>  
+              <Link href="/education/hop2/20/cern" className={style.generalLink}>  
+                   
+              </Link>  
+              <br />  
+              <h2 className={style.numberLink}>  
+                  7.6  
+              </h2>  
+              <Link href="/education/hop2/20/quant" className={style.generalLink}>  
+                 
+              </Link>
+ <h2 className={style.numberLink}>  
+                  7.7  
+              </h2>  
+              <Link href="/education/hop2/20/quant" className={style.generalLink}>  
+                 
+              </Link>
+              <br />  
        </> 
     );
 }
