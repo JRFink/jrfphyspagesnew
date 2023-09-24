281c1f3de123428471e22e2f2095255e078a07c0
Jeff F <jeff@jrfink.net>
2023-09-23 23:32:35 -0400

    a

--- pages/consulting/index.tsx
+++ pages/consulting/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import style from '@/styles/general.module.css';
 
 const Consult = () => { 
@@ -6,25 +7,25 @@ const Consult = () => {
         <h1 className={style.centerText}>
            Consulting 
         </h1>
- <p>
-  Welcome to the consulting page
-</p>
-<p> 
-    As part of the business we are going to offer some consulting services. 
-</p> 
-<p> 
-     Our main areas will be 
-<ul> 
-     <li>Physics education</li>
-     <li>Physics research</li>
-     <li>Higher ed</li>
-     <li>Education policy</li>
-     <li>Government research</li>
-</ul>
-</p>
-<p>
-Some sample research...
-</p>
+          <p>
+            Welcome to the consulting page
+          </p>
+          <p> 
+              As part of the business we are going to offer some consulting services. 
+          </p> 
+          <p> 
+              Our main areas will be 
+          <ul> 
+              <li>Physics education</li>
+              <li>Physics research</li>
+              <li>Higher ed</li>
+              <li>Education policy</li>
+              <li>Government research</li>
+          </ul>
+          </p>
+            <p>
+              Find some stats at the <Link href="https://nces.ed.gov">National center for education statistics</Link>.
+            </p>
       </>
   ); 
 } 
