e1bff0f0622a9aa1f944d905079bfb12eac7ef81
Jeff F <jeff@jrfink.net>
2023-09-24 15:37:05 -0400

    a

--- pages/education/hop1/west/index.tsx
+++ pages/education/hop1/west/index.tsx
@@ -9,6 +9,33 @@ const West = () => {
             <h1 className={style.centerText}>
                 Physics in the west
             </h1>
+            <h2 className={style.numberLink}>
+                2.1
+            </h2>
+            <Link href="/education/hop1/first/greece" className={style.generalLink}>
+                Physics in greece
+            </Link>
+            <br />
+            <h2 className={style.numberLink}>
+                2.2
+            </h2>
+            <Link href="/education/hop1/first/rome" className={style.generalLink}>
+                Physics in rome
+            </Link>
+            <br />
+            <h2 className={style.numberLink}>
+                2.3
+            </h2>
+            <Link href="/education/hop1/first/byz" className={style.generalLink}>
+                Physics in byzantine empire
+            </Link>
+            <br /> 
+            <h2 className={style.numberLink}>
+                2.4
+            </h2>
+            <Link href="/education/hop1/first/hre" className={style.generalLink}>
+                Physics in holy roman empire
+            </Link>
        </> 
     );
 }
