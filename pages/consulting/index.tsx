dd9a190555cb12d5c14b7254f3df31a3bb818b44
Jeff Fink <jeff@jrfink.net>
2023-09-23 20:49:35 -0400

    Create index.tsx

---
+++ pages/consulting/index.tsx
@@ -0,0 +1,12 @@
+import style from '@/styles/general.module.css';
+const Consult = () => { 
+  return ( 
+      <> 
+        <h1 className={style.centerText}>
+           Consulting 
+        </h1>
+      </>
+  ); 
+} 
+
+export default Consult;
