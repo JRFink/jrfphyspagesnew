17024c85e2a8c1132150e63d803d954d8d1cf6ff
Jeff Fink <fink.jeffr@gmail.com>
2023-09-24 17:51:41 -0400

    Update index.tsx

--- pages/education/hop2/20/index.tsx
+++ pages/education/hop2/20/index.tsx
@@ -9,6 +9,48 @@ const Twent = () => {
         <h1 className={style.centerText}>
             Development through 20th century
         </h1>
+<h2 className={style.numberLink}> 
+                 6.1 
+             </h2> 
+             <Link href="/education/hop2/20/feyn" className={style.generalLink}> 
+                 Feynman 
+             </Link> 
+             <br /> 
+             <h2 className={style.numberLink}> 
+                 6.2 
+             </h2> 
+             <Link href="/education/hop2/20/cold" className={style.generalLink}> 
+                 Cold war 
+             </Link> 
+             <br /> 
+             <h2 className={style.numberLink}> 
+                 6.3 
+             </h2> 
+             <Link href="/education/hop2/20/wein" className={style.generalLink}> 
+                 Weinberg
+             </Link> 
+             <br />  
+             <h2 className={style.numberLink}> 
+                 6.4 
+             </h2> 
+             <Link href="/education/hop2/20/dirac" className={style.generalLink}> 
+                 Dirac&#39;s equation 
+             </Link> 
+             <br /> 
+             <h2 className={style.numberLink}> 
+                 6.5 
+             </h2> 
+             <Link href="/education/hop2/20/cern" className={style.generalLink}> 
+                 Cern
+             </Link> 
+             <br /> 
+             <h2 className={style.numberLink}> 
+                 6.6 
+             </h2> 
+             <Link href="/education/hop2/20/quant" className={style.generalLink}> 
+                Quantum encryption
+             </Link>* 
+             <br />  
        </> 
     );
 }
