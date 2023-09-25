0add0ae5abdbbc87c82b1bb9a0eb628f694d57b7
Jeff Fink <fink.jeffr@gmail.com>
2023-09-25 17:06:20 -0400

    Update index.tsx

--- pages/research/gen/index.tsx
+++ pages/research/gen/index.tsx
@@ -280,7 +280,9 @@ const Gen = () => {
   <li><Link href="https://www.tsinghua.edu.cn/" className={style.noUnder}>Tsinghua University</Link></li>
   <li><Link href="https://www.pku.edu.cn/" className={style.noUnder}>Peking University</Link></li>
   <li><Link href="https://www.zju.edu.cn/mainm.htm" className={style.noUnder}>Zhejiang University</Link></li>
-</ul>                                    <br /> 
+</ul> 
+</p>
+                                   <br /> 
      
                             <p>
                                 There are about <Link href="http://en.moe.gov.cn/documents/reports/202304/t20230403_1054100.html">15,000</Link> senior high schools in china.
