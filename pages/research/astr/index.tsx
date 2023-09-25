bebdfb9c47b7b3cd0c81ac5cfb490f8f2f2839a7
Jeff F <jeff@jrfink.net>
2023-09-25 10:29:46 -0400

    w

--- pages/research/astr/index.tsx
+++ pages/research/astr/index.tsx
@@ -16,7 +16,7 @@ const Astr = () => {
             </div>
             <p>
                 Astrophysics studies the universe beyond earth. Some of the areas are galactic physics, cosmology, dark matter, and exoplanets. 
-                Find some of the latest research <a href="https://journals.aps.org/prresearch/subjects/astrophysics">here</a>.  
+                Find the latest research <a href="https://journals.aps.org/prresearch/subjects/astrophysics">here</a>.  
             </p>
             <br></br>
             <br></br>
@@ -30,7 +30,7 @@ const Astr = () => {
                 <Image
                     src={cosm}
                     style={{
-                        width: '60%',
+                        width: '70%',
                         borderRadius: "20px",
                         height: 'auto'
                     }}
@@ -38,6 +38,34 @@ const Astr = () => {
                     alt="A galaxy on a dark background" 
                 />
             </div>
+            <br /> 
+            <br />
+            <div className={style.flexContainer}>
+                <Link href="/research/astr#his" className={style.generalLink}>History</Link>
+                <br />
+                <Link href="/research/astr#theor" className={style.generalLink}>Theory</Link>
+                <br />
+                <Link href="/research/astr#exp" className={style.generalLink}>Experiment</Link>
+                <br />
+            </div>
+            <h2 className={style.h2SectionColor} id="his"> 
+                History
+            </h2>
+            <p>
+                History ... 
+            </p>
+            <h2 className={style.h2SectionColor} id="theor"> 
+                Theory
+            </h2>
+            <p>
+                Theory ... 
+            </p>
+            <h2 className={style.h2SectionColor} id="exp"> 
+                Experiment
+            </h2>
+            <p>
+                Experiment ... 
+            </p>
             <h2>
                 Locations
             </h2>
