98f7f68d42449ffe523ab5efd3095d358dac3a57
Jeff Fink <jeff@jrfink.net>
2023-09-27 11:24:24 -0400

    Create index.tsx

---
+++ pages/research/bio/equat/index.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'; 
+import Image from 'next/image';
+import style from '@/styles/general.module.css';
+
+const Equat = () => {
+    return (
+        <>
+            <br />
+            <Link href="/research/amo" className={style.backtoLink}>Biophysics</Link>
+            <br />
+            <br />
+            <h1 className={style.centerText}>
+                Equations
+            </h1>
+            <p>
+                Below you will find the equations you will use in biophysics. 
+            </p>
+            
+        </>
+    );
+}
+
+export default Equat;
