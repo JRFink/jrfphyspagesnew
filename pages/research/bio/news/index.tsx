8b487fbbad867f25ca80e57fab960901f67a0a63
Jeff Fink <jeff@jrfink.net>
2023-09-27 11:29:00 -0400

    Create index.tsx

---
+++ pages/research/bio/news/index.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'; 
+import Image from 'next/image';
+import style from '@/styles/general.module.css';
+
+const News = () => {
+    return (
+        <>
+            <br />
+            <Link href="/research/amo" className={style.backtoLink}>Biophys</Link>
+            <br />
+            <br />
+            <h1 className={style.centerText}>
+                Biophys news
+            </h1>
+            <br />
+           
+        </>
+    );
+}
+
+export default News;
