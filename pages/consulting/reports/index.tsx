202f9881c31db8dc7b48eb2014a8a50838d7b766
Jeff Fink <fink.jeffr@gmail.com>
2023-09-29 17:14:46 -0400

    Update index.tsx

--- pages/consulting/reports/index.tsx
+++ pages/consulting/reports/index.tsx
@@ -18,6 +18,7 @@ const Report = () => {
                     <li>Education policy in argentina</li>
                     <li>high school graduation rates in georgia</li>
                 </ul>
+  You can make a sample purchase <Link href="https://buy.stripe.com/28oeYtara5qhd5maEO">here</Link>
             </p>
         </>
     );
