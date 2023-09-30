4618198c5de11521807f4becf563434a8b7c49ef
Jeff Fink <fink.jeffr@gmail.com>
2023-09-29 23:13:30 -0400

    Update index.tsx

--- pages/education/em2/index.tsx
+++ pages/education/em2/index.tsx
@@ -10,6 +10,12 @@ const Em2 = () => {
             <h1 className={style.centerText}>
                 Electricity and Magnetism 2
             </h1>
+<br />
+<p> 
+  Here is a useful book for <Link href="https://www.amazon.com/gp/aw/d/0321856562/ref=dp_ob_neva_mobile">
+Electricity and magnetism</Link>
+</p>
+<br />
             <Link href="/education/em2/edynam" className={style.generalLink}>Electrodynamics</Link><br></br>
             <br></br>
             <Link href="/education/em2/cons" className={style.generalLink}>Conservation Laws</Link><br></br>
