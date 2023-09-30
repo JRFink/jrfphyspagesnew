f7f6ac0dd89270f4b3586f4a983c6476a445f97b
Jeff Fink <jeff@jrfink.net>
2023-09-30 17:13:42 -0400

    Create helpful.js

---
+++ pages/api/helpful.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');  
+  
+ const con = mysql.createConnection({ 
+  
+     host: process.env.DB_HOST, 
+     password: process.env.DB_PASS, 
+     user: process.env.DB_USER, 
+     database: process.env.DB_DATABASE, 
+     port: process.env.DB_PORT 
+  
+ }) 
+  
+ export default function handler(req, res) { 
+  
+  
+     const sql = "INSERT INTO "+process.env.UCH_UCH+" (helpful) VALUES ("+mysql.escape(req.body.helpfulSelection)+")";  
+  
+     con.query(sql, function(err, response) { 
+         if (err) { 
+             console.log(err); 
+             throw err;  
+         }; 
+         console.log('success'); 
+     }); 
+ };
