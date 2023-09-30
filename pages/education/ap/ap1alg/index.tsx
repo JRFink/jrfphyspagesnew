22b3e251bc2af4d2bdf21fc6ff33d881a141d839
Jeff Fink <fink.jeffr@gmail.com>
2023-09-30 01:02:26 -0400

    Update index.tsx

--- pages/education/ap/ap1alg/index.tsx
+++ pages/education/ap/ap1alg/index.tsx
@@ -12,26 +12,47 @@ const Ap1alg = () => {
         <br />
         <p> 
              The current structure of the class consists of seven units&#59; they are:
+</p>
 
-            <br></br> 
+            <br /> 
              <p className={style.numberLink}> 
                  1
              </p> 
              <Link href="/education/ap/apalg1/kin" className={style.generalLink}>Kinematics</Link><br></br> 
              <br></br> 
              <p className={style.numberLink}>
+             <br />  
+              <p className={style.numberLink}>  
+                  2 
+              </p>  
+              <Link href="/education/ap/ap1alg/dyn" className={style.generalLink}>Dynamics</Link><br></br>                
+              <br />  
+              <p className={style.numberLink}>  
+                  3
+              </p>  
+              <Link href="/education/ap/ap1alg/circ" className={style.generalLink}>Circular motion &#38; gravitation</Link><br></br>  
+              <br />  
+              <p className={style.numberLink}> 
+                  4 
+              </p>  
+              <Link href="/education/ap/ap1alg/energ" className={style.generalLink}>Energy</Link><br></br>  
+              <br />  
+              <p className={style.numberLink}> 
+                  5
+              </p>  
+              <Link href="/education/ap/ap1alg/energ" className={style.generalLink}>Momentum</Link><br></br>  
+              <br />   
+ <p className={style.numberLink}> 
+                  6
+              </p>  
+              <Link href="/education/ap/ap1alg/harm" className={style.generalLink}>Sime harmonic motion</Link><br></br>  
+              <br />  
 
- 
-<ol> 
-   <li>kinematics</li>
-   <li>dynamics</li>
-   <li>circular motion and gravitation</li>
-   <li>energy</li>
-   <li>momentum</li>
-   <li>simple harmonic motion</li>
-   <li>torque and rotational motion</li>
-</ol> 
-        </p> 
+ <p className={style.numberLink}> 
+                  7 
+              </p>  
+              <Link href="/education/ap/ap1alg/torq" className={style.generalLink}>Torque and rotational motion</Link><br></br>  
+              <br />  
         <p> 
             Find the curriculum 
             <a href="https://apstudents.collegeboard.org/courses/ap-physics-1-algebra-based"> here</a>.
