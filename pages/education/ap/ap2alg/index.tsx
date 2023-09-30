858a6ce3db352ac08dc94b934c9e3ac5f9eb3b0f
Jeff Fink <fink.jeffr@gmail.com>
2023-09-30 01:10:55 -0400

    Update index.tsx

--- pages/education/ap/ap2alg/index.tsx
+++ pages/education/ap/ap2alg/index.tsx
@@ -19,52 +19,40 @@ The current structure of the class consists of seven units&#59; they are:
               <p className={style.numberLink}>  
                   1 
               </p>  
-              <Link href="/education/ap/apalg1/fluid" className={style.generalLink}>Fluids: pressure and forces</Link><br></br>  
+              <Link href="/education/ap/ap2alg/fluid" className={style.generalLink}>Fluids: pressure and forces</Link><br></br>  
               <br></br>  
               <p className={style.numberLink}> 
               <br />   
                <p className={style.numberLink}>   
                    2  
                </p>   
-               <Link href="/education/ap/ap1alg/dyn" className={style.generalLink}>Dynamics</Link><br></br>                 
+               <Link href="/education/ap/ap2alg/therm" className={style.generalLink}>Thermodynamics</Link><br></br>                 
                <br />   
                <p className={style.numberLink}>   
                    3 
                </p>   
-               <Link href="/education/ap/ap1alg/circ" className={style.generalLink}>Circular motion &#38; gravitation</Link><br></br>   
+               <Link href="/education/ap/ap2alg/elec" className={style.generalLink}>Electric force, field &#38; potential</Link><br></br>   
                <br />   
                <p className={style.numberLink}>  
                    4  
                </p>   
-               <Link href="/education/ap/ap1alg/energ" className={style.generalLink}>Energy</Link><br></br>   
+               <Link href="/education/ap/ap2alg/circ" className={style.generalLink}>Electric circuits</Link><br></br>   
                <br />   
                <p className={style.numberLink}>  
                    5 
                </p>   
-               <Link href="/education/ap/ap1alg/energ" className={style.generalLink}>Momentum</Link><br></br>   
+               <Link href="/education/ap/ap2alg/mag" className={style.generalLink}>Magnetism and electromagnetic induction</Link><br></br>   
                <br />    
   <p className={style.numberLink}>  
                    6 
                </p>   
-               <Link href="/education/ap/ap1alg/harm" className={style.generalLink}>Sime harmonic motion</Link><br></br>   
-               <br />   
-  
+               <Link href="/education/ap/ap2alg/geo" className={style.generalLink}>Geometric and physical objects</Link><br></br>   
+               <br />     
   <p className={style.numberLink}>  
                    7  
                </p>   
-               <Link href="/education/ap/ap1alg/torq" className={style.generalLink}>Torque and rotational motion</Link><br></br>   
-               <br />  
-
-<ol> 
-     <li>fluids: pressure and forces</li>
-     <li>thermodynamics</li>
-     <li>electric force, field, and potential</li> 
-     <li>electric circuits</li>
-     <li>magnetism and electromagnetic       induction</li>
-     <li>geometric and physical optics</li>
-     <li>quantum, atomic, and nuclear physics</li> 
-</ol>
-</p>
+               <Link href="/education/ap/ap2alg/quant" className={style.generalLink}>Quantum, atomic, and nuclear physics</Link><br></br>   
+               <br /> 
          <p>  
             Find the curriculum  
             <a href="https://apstudents.collegeboard.org/courses/ap-physics-2-algebra-based"> here</a>. 
