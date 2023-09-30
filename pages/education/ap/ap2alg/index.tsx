98c0a973c99a5873b54f23010fae6054bd7c023f
Jeff Fink <fink.jeffr@gmail.com>
2023-09-30 01:04:26 -0400

    Update index.tsx

--- pages/education/ap/ap2alg/index.tsx
+++ pages/education/ap/ap2alg/index.tsx
@@ -12,6 +12,48 @@ const Ap2alg = () => {
         <br /> 
 <p> 
 The current structure of the class consists of seven units&#59; they are:
+</p> 
+
+
+            <br />  
+              <p className={style.numberLink}>  
+                  1 
+              </p>  
+              <Link href="/education/ap/apalg1/fluid" className={style.generalLink}>Fluids: pressure and forces</Link><br></br>  
+              <br></br>  
+              <p className={style.numberLink}> 
+              <br />   
+               <p className={style.numberLink}>   
+                   2  
+               </p>   
+               <Link href="/education/ap/ap1alg/dyn" className={style.generalLink}>Dynamics</Link><br></br>                 
+               <br />   
+               <p className={style.numberLink}>   
+                   3 
+               </p>   
+               <Link href="/education/ap/ap1alg/circ" className={style.generalLink}>Circular motion &#38; gravitation</Link><br></br>   
+               <br />   
+               <p className={style.numberLink}>  
+                   4  
+               </p>   
+               <Link href="/education/ap/ap1alg/energ" className={style.generalLink}>Energy</Link><br></br>   
+               <br />   
+               <p className={style.numberLink}>  
+                   5 
+               </p>   
+               <Link href="/education/ap/ap1alg/energ" className={style.generalLink}>Momentum</Link><br></br>   
+               <br />    
+  <p className={style.numberLink}>  
+                   6 
+               </p>   
+               <Link href="/education/ap/ap1alg/harm" className={style.generalLink}>Sime harmonic motion</Link><br></br>   
+               <br />   
+  
+  <p className={style.numberLink}>  
+                   7  
+               </p>   
+               <Link href="/education/ap/ap1alg/torq" className={style.generalLink}>Torque and rotational motion</Link><br></br>   
+               <br />  
 
 <ol> 
      <li>fluids: pressure and forces</li>
