c5fc3b5d58ae60c4c597aea63c133f05b0089d72
Jeff Fink <jeff@jrfink.net>
2023-10-01 04:13:26 -0400

    Create index.tsx

---
+++ pages/education/cm1/stuff/index.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'; 
+ import style from '@/styles/general.module.css'; 
+  
+ const Stuff = () => { 
+     return ( 
+         <> 
+             <br /> 
+             <Link href="/education/cm1" className={style.backtoLink}>Class mech 1</Link> 
+             <br /> 
+             <h1 className={style.centerText}> 
+                 Stuff you should know 
+             </h1> 
+         </> 
+     ); 
+ } 
+  
+ export default Stuff;
