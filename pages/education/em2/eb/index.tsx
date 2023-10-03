150cfb447c899e2a1d71d98583a04a8d4dad3167
Jeff F <jeff@jrfink.net>
2023-10-03 00:00:31 -0400

    s

--- pages/education/em2/eb/index.tsx
+++ pages/education/em2/eb/index.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import style from '@/styles/general.module.css';
+import emwaves from '@/public/images/emwave.svg';
 import EbEx from '@/pages/exercises/em2/ebex';
 import Helpful from '@/pages/components/helpful';
 
@@ -11,6 +13,23 @@ const Eb = () => {
             <h1 className={style.centerText}>
                 Electromagnetic Waves
             </h1>
+            <div  
+                style={{ 
+                    display: "flex", 
+                    justifyContent: "center", 
+                }} 
+            > 
+                <Image 
+                    src={emwaves} 
+                    style={{ 
+                        width: '80%', 
+                        borderRadius: "20px", 
+                        height: 'auto' 
+                    }} 
+                    sizes="100vw" 
+                    alt="Large blue waves mixed with orange smaller circles"  
+                /> 
+            </div>
             <EbEx />
             <Helpful />
         </>
