82cd0b0805218c7c48448ecccf8ada055b814ee2
Jeff Fink <fink.jeffr@gmail.com>
2023-10-04 22:19:50 -0400

    Update index.tsx

--- pages/education/gp1/exam/index.tsx
+++ pages/education/gp1/exam/index.tsx
@@ -5,7 +5,7 @@ const Exam = () => {
   return (
      <>
          <br /> 
-         <Link href="/education/gp1" className={style.backToLink}>Gen phys 1</Link>
+         <Link href="/education/gp1" className={style.backtoLink}>Gen phys 1</Link>
          <br />
          <h1 className={style.centerText}>
              Exams 
