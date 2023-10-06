974416b1a2b0ddd7e2b2c716c59fc4f75a359789
Jeff Fink <jeff@jrfink.net>
2023-10-06 12:00:05 -0400

    Create index.tsx

---
+++ pages/education/gp1/ideal/index.tsx
@@ -0,0 +1,10 @@
+import style from '@/styles/general.module.css';
+
+const Id = () => { 
+  return (
+    <h1 className={style.centerText}>
+    Idealized models
+    </h1>
+    );
+} 
+export default Id;
