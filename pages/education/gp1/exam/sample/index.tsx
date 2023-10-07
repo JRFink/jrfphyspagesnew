7168fcddfe37a8c413c299427cfa554457de55ff
Jeff Fink <fink.jeffr@gmail.com>
2023-10-07 17:16:59 -0400

    Update index.tsx

--- pages/education/gp1/exam/sample/index.tsx
+++ pages/education/gp1/exam/sample/index.tsx
@@ -9,14 +9,54 @@ const Sample = () => {
       <h2>
           Multiple choice 
       </h2>
+         <p>
+             <ul>
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>                 
+                 <li>A</li>
+                 <li>B</li>
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>                 
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>                 
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>                 
+                 <li>A</li>    
+             </ul>
+         </p>
       <br />
       <h2>
           Short response
       </h2>
+       <p>
+             <ul>
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>
+                 <li>A</li>                 
+                 <li>A</li>
+             </ul>
+       </p> 
       <br />
       <h2>
          Long response
       </h2>
+        <p> 
+          <ul>
+              <li>Describe...</li>
+              <li>Explain...</li>
+              <li>Connect...</li>
+          </ul>
+        </p>
    </>
   );
 }
