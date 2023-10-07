3cff260433287dd7a5fb8e89c22c140590766098
Jeff Fink <jeff@jrfink.net>
2023-10-07 17:00:50 -0400

    Create index.tsx

---
+++ pages/education/gp1/exam/sample/index.tsx
@@ -0,0 +1,22 @@
+import style from '@/styles/general.module.css';
+
+const Sample = () => { 
+   return (
+    <> 
+      <h1 className={style.centerText}>
+         Sample exam 
+      </h1>
+      <h2>
+          Multiple choice 
+      </h2>
+      <br />
+      <h2>
+          Short response
+      </h2>
+      <br />
+      <h2>
+         Long response
+      </h2>
+   </>
+  );
+}
