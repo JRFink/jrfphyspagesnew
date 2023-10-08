4372017568b6c51cd6d2e28ac773744c02a4ab21
Jeff F <jeff@jrfink.net>
2023-10-08 00:39:01 -0400

    w

--- pages/education/gp1/ideal/index.tsx
+++ pages/education/gp1/ideal/index.tsx
@@ -1,15 +1,19 @@
+import Link from 'next/link';
 import style from '@/styles/general.module.css';
 
 const Id = () => { 
   return (
-<>
-    <h1 className={style.centerText}>
-    Idealized models
-    </h1>
-   <p>
-Models are useful because they help us apply the same principles to a range of phenomena.
-</p>
-</>
+    <>
+        <br />
+        <Link href="/education/gp1" className={style.backtoLink}>Gen phys 1</Link>
+        <br />
+        <h1 className={style.centerText}>
+          Idealized models
+        </h1>
+          <p>
+            Models are useful because they help us apply the same principles to a range of phenomena.
+          </p>
+    </>
     );
 } 
 export default Id;
