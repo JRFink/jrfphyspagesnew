efea8e6b970f0f1f4ef03f1fd4fd1921f48a7f2a
Jeff Fink <fink.jeffr@gmail.com>
2023-10-08 00:33:13 -0400

    Update index.tsx

--- pages/education/gp1/ideal/index.tsx
+++ pages/education/gp1/ideal/index.tsx
@@ -2,12 +2,14 @@ import style from '@/styles/general.module.css';
 
 const Id = () => { 
   return (
+<>
     <h1 className={style.centerText}>
     Idealized models
     </h1>
    <p>
 Models are useful because they help us apply the same principles to a range of phenomena.
 </p>
+</>
     );
 } 
 export default Id;
