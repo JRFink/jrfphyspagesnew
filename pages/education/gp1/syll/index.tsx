645369fa2372aa58ff5a198f455f5834eef66169
Jeff Fink <fink.jeffr@gmail.com>
2023-10-07 22:14:30 -0400

    Update index.tsx

--- pages/education/gp1/syll/index.tsx
+++ pages/education/gp1/syll/index.tsx
@@ -4,6 +4,9 @@ const Syll = () => {
 
   return (
      <>
+<br />
+<Link href="/education/gp1" className={style.backtoLink}>Gen phys 1</Link>
+<br />
        <h1 className={style.centerText}>
           Syllabus 
        </h1>
