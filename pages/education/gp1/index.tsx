2b2da7b2e1ad54ff5f8d8e4a20fd14655c645f63
Jeff Fink <fink.jeffr@gmail.com>
2023-10-07 22:16:34 -0400

    Update index.tsx

--- pages/education/gp1/index.tsx
+++ pages/education/gp1/index.tsx
@@ -20,45 +20,7 @@ const Gp1 = () => {
                 I don&#39;t have all the implementation yet, but I have the topical structure. 
                 Take a look below. 
             </p>
-            <h2>
-                Description
-            </h2>
-            <p>
-                An introduction to general physics. It will cover the following topics: 
-                <br></br>
-                <br></br>
-                &nbsp; &nbsp; kinematics, forces &#40;newton&#39;s laws&#41;, work, energy, rotational motion, taylor expansion, gravity, waves, fluid motion, and intro thermodynamics. 
-                <br></br><br></br>
-                The class consists of ~ 20 modules, which consist of lecture videos, homework assignments, quizzes, exams, an extra credit paper, and a final exam.
-                I hope you enjoy it, and please give me your <Link href="/contact">feedback</Link> as you go through it.
-                <br></br>
-                <br></br>
-          
-            </p>
-            <h2>
-                Prerequisites
-            </h2>
-            <p>
-                Algebra, geometry, trigonometry
-            </p>
-            <h2>
-                Course number
-            </h2>
-            <p>
-                Phys 101
-            </p>
-            <h2>
-                Location
-            </h2>
-            <p>
-                Asynchronous online
-            </p>
-            <h2>
-                Instructor
-            </h2>
-            <p>
-                Jeff Fink
-            </p>
+         
             <h2>
                 Syllabus
             </h2>
