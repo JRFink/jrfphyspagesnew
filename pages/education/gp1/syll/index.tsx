f0ed65ffd9f4f0b7ca1bfb7bfc5ed947d57e5a00
Jeff Fink <fink.jeffr@gmail.com>
2023-10-07 20:01:41 -0400

    Update index.tsx

--- pages/education/gp1/syll/index.tsx
+++ pages/education/gp1/syll/index.tsx
@@ -1,11 +1,22 @@
+import style from '@/styles/general.module.css';
+
 const Syll = () => { 
 
   return (
-    <>
-    <h1>
-    Syllabus 
-    </h1>
-    </>
+     <>
+       <h1 className={style.centerText}>
+          Syllabus 
+       </h1>
+      <h2>
+           Grading 
+      </h2>
+      <p>
+          Total course points is 1&#44;000. Division is:
+<ul>
+   <li>Homework - 250 pts</li>
+</ul>
+      </p>
+     </>
     );
 } 
 export default Syll;
