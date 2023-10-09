a1f809f0e4edc5d90ccd8ccc72fc0ddc94fbe34a
Jeff Fink <fink.jeffr@gmail.com>
2023-10-09 10:51:01 -0400

    Update index.tsx

--- pages/education/gp1/rm/hw/index.tsx
+++ pages/education/gp1/rm/hw/index.tsx
@@ -11,6 +11,21 @@ const RotHw = () => {
             <h1 className={style.centerText}>
                 Extended bodies and rotation hw
             </h1>
+   <p> 
+ <ul> 
+      <li>Angular position</li>
+      <li>Angular velocity</li>  
+      <li>Angular acceleration </li> 
+      <li>Position and angular position</li> 
+      <li>Speed and angular speed</li> 
+      <li>Acceleration and angular acceleration</li> 
+      <li>Uniform circular motion</li> 
+      <li>Non-uniform circular motion</li>
+      <li>Moment of inertia</li>
+      <li>Parallel-axis theorem</li>
+      <li>Energy</li>
+ </ul> 
+ </p>
         </>
     );
 }
