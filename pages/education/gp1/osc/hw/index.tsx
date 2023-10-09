b6f8d06e1cf2a48fcb93a117f5b85b7ec649c57c
Jeff F <jeff@jrfink.net>
2023-10-09 10:04:11 -0400

    a

--- pages/education/gp1/osc/hw/index.tsx
+++ pages/education/gp1/osc/hw/index.tsx
@@ -11,6 +11,18 @@ const OscHw = () => {
             <h1 className={style.centerText}>
                 Oscillations hw
             </h1>
+            {/* <p>
+                <ul>
+                    <li>Gravitational field</li>
+                    <li>Gravitational force</li>
+                    <li>Gravitational potential</li>
+                    <li>Gravitation near earth&#39;s surface</li>
+                    <li>Gravitation at distance of satellites</li>
+                    <li>Motion of satellites</li>
+                    <li>Kepler&#39;s laws</li>
+                    <li>Einstein and gravity</li>
+                </ul>
+            </p> */}
         </>
     );
 }
