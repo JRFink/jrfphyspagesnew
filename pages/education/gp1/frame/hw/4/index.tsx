49e4fe3b4dc8696d2fa5f6930f5ececd3a7f9f69
Jeff F <jeff@jrfink.net>
2023-10-09 22:03:31 -0400

    a

--- pages/education/gp1/frame/hw/4/index.tsx
+++ pages/education/gp1/frame/hw/4/index.tsx
@@ -1,17 +1,16 @@
 import Link from 'next/link';
 import style from '@/styles/general.module.css';
 
-const FrameHw = () => {
+const Frame4 = () => {
     return (
         <>
             <br />
-            <Link href="/education/gp1/hw" className={style.backtoLink}>Homework</Link><br></br>
-            <br></br>
+            <Link href="/education/gp1/hw" className={style.backtoLink}>Homework</Link>
             <br></br>
             <h1 className={style.centerText}>
                 Frame hw
             </h1>
-            <div className={style.flexContainer}>
+                <div className={style.flexContainer}>
                 <Link href="/education/gp1/frame/hw/1" className={style.generalLink}>1</Link>
                 <br />
                 <Link href="/education/gp1/frame/hw/2" className={style.generalLink}>2</Link>
@@ -24,21 +23,10 @@ const FrameHw = () => {
                 <br />
             </div>
             <p>
-                <ul>
-                    <li>Frames</li>
-                    <li>Your frame</li>
-                    <li>Relative frame</li>
-                    <li>Inertial and non-inertial frame</li>
-                    <li>Units</li>
-                    <li>Dimensional analysis</li>
-                    <li>Gov standards</li>
-                    <li>Measurement</li>
-                    <li>Significant figures</li>
-                    <li>Uncertainty</li>
-                </ul>
+                Some hw 4
             </p>
         </>
     );
 }
 
-export default FrameHw;
+export default Frame4;
