2e359a34598f9f5b0f3d7a15ecdfaf6dbe323f76
Jeff Fink <fink.jeffr@gmail.com>
2023-10-10 11:49:06 -0400

    Update hwnavb.tsx

--- pages/education/gp1/1dm/hw/hwnav/hwnavb.tsx
+++ pages/education/gp1/1dm/hw/hwnav/hwnavb.tsx
@@ -5,8 +5,6 @@ const Hwheader = () => {
     return (
         <>
             <br /> 
-            <br /> 
-            <br />
             <Link href="/education/gp1/hw" className={style.backtoLink} id="head">Homework</Link>
             <br></br>
             <h1 className={style.centerText}>
