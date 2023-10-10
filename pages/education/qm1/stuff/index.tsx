5332a6a223c8819e1f5ae58e40e274735c83d6e0
Jeff Fink <fink.jeffr@gmail.com>
2023-10-10 17:32:39 -0400

    Update index.tsx

--- pages/education/qm1/stuff/index.tsx
+++ pages/education/qm1/stuff/index.tsx
@@ -8,8 +8,11 @@ import style from '@/styles/general.module.css';
              <Link href="/education/qm1" className={style.backtoLink}>Quantum 1</Link> 
              <br /> 
              <h1 className={style.centerText}> 
-                 Stuff you should know 
+                 Question database
              </h1> 
+<p>
+    Hello, and welcome to the question database. This is your one-stop-shop for all your quantum 1 questions. Eventually we will have hundreds of quantum questions for you to do.
+</p>
          </> 
      ); 
  } 
