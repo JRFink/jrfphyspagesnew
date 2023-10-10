e7057b35b12572399af5bdf41c7eb91a0436e297
Jeff F <jeff@jrfink.net>
2023-10-09 23:21:36 -0400

    a

---
+++ pages/education/gp1/diff/hw/1/index.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import style from '@/styles/general.module.css';
+
+const Diff1 = () => {
+    return (
+        <>
+            <br />
+            <Link href="/education/gp1/hw" className={style.backtoLink}>Homework</Link>
+            <br></br>
+            <h1 className={style.centerText}>
+                Differential equations hw
+            </h1>
+                <div className={style.flexContainer}>
+                <Link href="/education/gp1/diff/hw/1" className={style.yellowLink}>1</Link>
+                <br />
+                <Link href="/education/gp1/diff/hw/2" className={style.generalLink}>2</Link>
+                <br />
+                <Link href="/education/gp1/diff/hw/3" className={style.generalLink}>3</Link>
+                <br />
+                <Link href="/education/gp1/diff/hw/4" className={style.generalLink}>4</Link>
+                <br />
+                <Link href="/education/gp1/diff/hw/5" className={style.generalLink}>5</Link>
+                <br />
+            </div>
+            <p>
+                <ul>
+                    <li>First order</li>
+                    <li>Second order</li>
+                </ul>
+            </p>
+        </>
+    );
+}
+
+export default Diff1;
---
+++ pages/education/gp1/eq/hw/5/index.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import style from '@/styles/general.module.css';
+
+const Eq5 = () => {
+    return (
+        <>
+            <br />
+            <Link href="/education/gp1/hw" className={style.backtoLink}>Homework</Link>
+            <br />
+            <h1 className={style.centerText}>
+                Equilibrium and elasticity hw
+            </h1>
+                <div className={style.flexContainer}>
+                <Link href="/education/gp1/eq/hw/1" className={style.generalLink}>1</Link>
+                <br />
+                <Link href="/education/gp1/eq/hw/2" className={style.generalLink}>2</Link>
+                <br />
+                <Link href="/education/gp1/eq/hw/3" className={style.generalLink}>3</Link>
+                <br />
+                <Link href="/education/gp1/eq/hw/4" className={style.generalLink}>4</Link>
+                <br />
+                <Link href="/education/gp1/eq/hw/5" className={style.yellowLink}>5</Link>
+                <br />
+            </div>
+            <p>
+                Some hw 5
+            </p>
+            <br /> 
+            <br />
+            <div className={style.centerInADiv}>
+                <button className={style.greenButton}>Submit hw</button>
+            </div>
+        </>
+    );
+}
+
+export default Eq5;
