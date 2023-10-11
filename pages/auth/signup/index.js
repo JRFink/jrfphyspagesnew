83875b3a72e53639b1a6f08e967f70043e86cc7c
Jeff Fink <fink.jeffr@gmail.com>
2023-10-11 11:41:51 -0400

    Update index.js

--- pages/auth/signup/index.js
+++ pages/auth/signup/index.js
@@ -121,6 +121,8 @@ const SignupFlow = () => {
                             We&#39;re thinking &#36;5 &#47; month. < br /> 
                         <br />
                         Find your school, match requirements, get credit. 
+<br /> 
+<br />
                 <input type="text" className={style.searchBox} placeholder="Find your school"></input>
                         </p> 
                         <br />
