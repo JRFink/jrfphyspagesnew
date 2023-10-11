fd77041ede7431f36e59274655732e94e9b8db2c
Jeff F <jeff@jrfink.net>
2023-10-11 08:39:58 -0400

    a

--- pages/education/gp1/slaw/hw/1/index.tsx
+++ pages/education/gp1/slaw/hw/1/index.tsx
@@ -1,42 +1,51 @@
 import Link from 'next/link';
 import style from '@/styles/general.module.css';
+import Hwheader from '../hwnav/hwnavb';
 
-const Slaw1 = () => {
+const Onedm1 = () => {
     return (
         <>
-            <br />
-            <Link href="/education/gp1/hw" className={style.backtoLink}>Homework</Link>
-            <br></br>
-            <h1 className={style.centerText}>
-                Second law hw
-            </h1>
-                <div className={style.flexContainer}>
-                <Link href="/education/gp1/slaw/hw/1" className={style.yellowLink}>1</Link>
+            <Hwheader />
+            <div className={style.flexContainer}>
+                <Link href="/education/gp1/slaw/hw/1#head" className={style.yellowLink}>1</Link>
                 <br />
-                <Link href="/education/gp1/slaw/hw/2" className={style.generalLink}>2</Link>
+                <Link href="/education/gp1/slaw/hw/2#head" className={style.generalLink}>2</Link>
                 <br />
-                <Link href="/education/gp1/slaw/hw/3" className={style.generalLink}>3</Link>
+                <Link href="/education/gp1/slaw/hw/3#head" className={style.generalLink}>3</Link>
                 <br />
-                <Link href="/education/gp1/slaw/hw/4" className={style.generalLink}>4</Link>
+                <Link href="/education/gp1/slaw/hw/4#head" className={style.generalLink}>4</Link>
                 <br />
-                <Link href="/education/gp1/slaw/hw/5" className={style.generalLink}>5</Link>
+                <Link href="/education/gp1/slaw/hw/5#head" className={style.generalLink}>5</Link>
+                <br />
+                <Link href="/education/gp1/slaw/hw/6#head" className={style.generalLink}>6</Link>
+                <br />
+                <Link href="/education/gp1/slaw/hw/7#head" className={style.generalLink}>7</Link>
+                <br />
+                <Link href="/education/gp1/slaw/hw/8#head" className={style.generalLink}>8</Link>
+                <br />
+                <Link href="/education/gp1/slaw/hw/9#head" className={style.generalLink}>9</Link>
+                <br />
+                <Link href="/education/gp1/slaw/hw/10#head" className={style.generalLink}>10</Link>
+                <br />
+                <Link href="/education/gp1/slaw/hw/11#head" className={style.generalLink}>11</Link>
                 <br />
             </div>
             <p>
-                <ul>  
-                    <li>Entropy</li>
-                    <li>Engines</li> 
-                    <li>Refrigerators</li>   
-                    <li>Statistics</li>  
-                </ul>  
+                You can submit hw on the last question.
             </p>
+            <h2>
+                Entropy
+            </h2>
             <h3 className={style.h3num}>
                 A
             </h3>
             <br />
             &nbsp; &nbsp; <label>Answer: </label>
             <input type="text"></input>
+            <br />
+            <br />
             <button className={style.greenButton}>Check answer</button>
+            <br />
             <br />
             <h3 className={style.h3num}>
                 B
@@ -44,7 +53,10 @@ const Slaw1 = () => {
             <br />
             &nbsp; &nbsp; <label>Answer: </label>
             <input type="text"></input>
+            <br />
+            <br />
             <button className={style.greenButton}>Check answer</button>
+            <br />
             <br />
             <h3 className={style.h3num}>
                 C
@@ -52,7 +64,10 @@ const Slaw1 = () => {
             <br />
             &nbsp; &nbsp; <label>Answer: </label>
             <input type="text"></input>
+            <br />
+            <br />
             <button className={style.greenButton}>Check answer</button>
+            <br />
             <br />
             <h3 className={style.h3num}>
                 D
@@ -60,9 +75,11 @@ const Slaw1 = () => {
             <br />
             &nbsp; &nbsp; <label>Answer: </label>
             <input type="text"></input>
+            <br />
+            <br />
             <button className={style.greenButton}>Check answer</button>
         </>
     );
 }
 
-export default Slaw1;
+export default Onedm1;
