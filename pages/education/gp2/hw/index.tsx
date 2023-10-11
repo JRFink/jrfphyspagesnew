e24b84c188a603e61ab6f65b14b3f07245e2faeb
Jeff F <jeff@jrfink.net>
2023-10-11 09:55:33 -0400

    a

--- pages/education/gp2/hw/index.tsx
+++ pages/education/gp2/hw/index.tsx
@@ -7,36 +7,39 @@ const Hw = () => {
             <h1 className={style.centerText}>
                 Homework
             </h1>
-            <Link href="/education/gp2/ac/hw/1" className={style.lightGreenLink}>AC circuits</Link>
-            <br />
-            <br />
-            <Link href="/education/gp2/b/hw/1" className={style.lightGreenLink}>Magnetic field and force</Link>
-            <br />
-            <br />
-            <Link href="/education/gp2/cap/hw/1" className={style.lightGreenLink}>Electrostatic energy and capacitors</Link>
-            <br />
-            <br />
-            <Link href="/education/gp2/dc/hw/1" className={style.lightGreenLink}>DC circuits</Link>
-            <br />
-            <br />
-            <Link href="/education/gp2/eb/hw/1" className={style.lightGreenLink}>Electromagnetic waves</Link>
-            <br />
-            <br />
             <Link href="/education/gp2/elec/hw/1" className={style.lightGreenLink}>Electric chage, force, and field</Link>
-            <br />
-            <br />
-            <Link href="/education/gp2/emf/hw/1" className={style.lightGreenLink}>Electromagnetic induction</Link>
             <br />
             <br />
             <Link href="/education/gp2/gauss/hw/1" className={style.lightGreenLink}>Gauss&#39; law</Link>
             <br />
             <br />
-            <Link href="/education/gp2/i/hw/1" className={style.lightGreenLink}>Electric currents</Link>
+            <Link href="/education/gp2/vq/hw/1" className={style.lightGreenLink}>Electric potential</Link>
+            <br />
+            <br />
+            <Link href="/education/gp2/cap/hw/1" className={style.lightGreenLink}>Electrostatic energy and capacitors</Link>
+            <br />
+            <br />
+            <Link href="/education/gp2/i/hw/1" className={style.lightGreenLink}>Electric current</Link>
+            <br />
+            <br />
+            <Link href="/education/gp2/dc/hw/1" className={style.lightGreenLink}>DC circuits</Link>
+            <br />
+            <br />
+            <Link href="/education/gp2/b/hw/1" className={style.lightGreenLink}>Magnetic field and force</Link>
+            <br />
+            <br />
+            <Link href="/education/gp2/emf/hw/1" className={style.lightGreenLink}>Electromagnetic induction</Link>
+            <br />
+            <br />
+            <Link href="/education/gp2/ac/hw/1" className={style.lightGreenLink}>AC circuits</Link>
+            <br />
+            <br />
+            <Link href="/education/gp2/eb/hw/1" className={style.lightGreenLink}>Electromagnetic waves</Link>
             <br />
             <br />
             <Link href="/education/gp2/max/hw/1" className={style.lightGreenLink}>Maxwell&#39;s equations</Link>
             <br />
-            
+            <br />
         </>
     );
 }
