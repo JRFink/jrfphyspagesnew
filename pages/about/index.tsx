d13c48e4c3c8568e25d5526ea921a18cee4b769f
Jeff Fink <fink.jeffr@gmail.com>
2023-10-12 17:09:07 -0400

    Update index.tsx

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -35,7 +35,7 @@ const About = () => {
         <ul>
              <li>Selling general physics classes</li>
              <li>Selling upper level physics classes</li>
-             <li>Synthesizing current physics research into an easy-to-navigate <Link href="/research/qis/news">news</Link> tab</li>
+             <li>Synthesizing current physics research into an easy-to-navigate <Link href="/research/news">news</Link> tab</li>
              <li>Selling <Link href="/consulting/reports">informational reports</Link> to higher learning institutions, governments, etc. about higher ed policy, physics, etc. </li>
         </ul>
    </p>
