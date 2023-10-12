958e4f31a3bf02ac3e600fbce93a3eb1ac9d5d32
Jeff Fink <fink.jeffr@gmail.com>
2023-10-12 17:19:52 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -90,6 +90,8 @@ const indexPage = () => {
 <p>
    On a scale of 1 - 5, how are you enjoying the site? 
 <br />
+<br />
+<br />
 <div className={style.flexContainer}>
 <button>1</button> &nbsp; &nbsp; <button>2</button> &nbsp;  &nbsp; <button>3</button> &nbsp; &nbsp; <button>4</button> &nbsp; &nbsp; <button>5</button>
 </div>
