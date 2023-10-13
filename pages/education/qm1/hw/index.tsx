574a6a902ab39e46df19357a470432fb47ec7618
Jeff F <jeff@jrfink.net>
2023-10-13 00:04:08 -0400

    a

---
+++ pages/education/qm1/hw/index.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'; 
+import style from '@/styles/general.module.css';
+
+const Hw = () => {
+    return (
+        <>
+            <h1 className={style.centerText}>
+                Homework
+            </h1>
+            <br />
+            <br />
+            <Link href="/education/qm1/gerlach/hw" className={style.lightGreenLink}>From Experiment to Theory - Stern Gerlach</Link><br></br>
+            <br></br>
+            <Link href="/education/qm1/op/hw" className={style.lightGreenLink}>Linear algebra crash course</Link><br></br>
+            <br></br>
+            <Link href="/education/qm1/dxdp/hw" className={style.lightGreenLink}>Measurement and Uncertainty</Link><br></br>
+            <br></br>
+            <Link href="/education/qm1/ih/hw" className={style.lightGreenLink}>Schrodinger Equation</Link><br></br>
+            <br></br>
+            <Link href="/education/qm1/box/hw" className={style.lightGreenLink}>Particle in a Box</Link><br></br>
+            <br></br>
+            <Link href="/education/qm1/jls/hw" className={style.lightGreenLink}>Angular Momentum</Link><br></br>
+            <br></br>
+            <Link href="/education/qm1/ho/hw" className={style.lightGreenLink}>Harmonic Oscillator</Link><br></br>
+        </>
+    );
+}
+
+export default Hw;
