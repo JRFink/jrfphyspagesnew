39c0074ceab6b613b36c9d6bb7071670f332baaa
Jeff Fink <jeff@jrfink.net>
2023-10-15 02:29:03 -0400

    Create index.tsx

---
+++ pages/problems/index.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'; 
+ import style from '@/styles/general.module.css'; 
+  
+ const Prob = () => {  
+   return ( 
+     <> 
+       <h1 className={style.centerText}> 
+         Problems
+      </h1> 
+       <p> 
+            Here are some unsolved problems in physics -   
+       </p> 
+       <p>
+           <ul>
+               <li>Tangential velocity of matter around galaxies - dark matter?</li>
+               <li>Connection between the gravitational force and the electric force</li>
+               <li>Gravity in the standard model</li>
+           </ul>
+       </p>
+     </> 
+   ); 
+ } 
+  
+ export defualt Prob;
