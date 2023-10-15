0512baadd18f55daa86fa07d0ce170cd0695770d
Jeff Fink <jeff@jrfink.net>
2023-10-15 01:24:30 -0400

    Create index.tsx

---
+++ pages/community/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import style from '@/styles/general.module.css';
+
+const Comm = () => { 
+    return (
+      <h1 className={style.centerText}>
+          Community
+      </h1>
+      <p>
+         Hello and welcome to the physics community! 
+      </p>
+    );
+}
+
+export default Comm;
