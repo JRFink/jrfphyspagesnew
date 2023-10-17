b74eecf3e8addc28fa3772cdefac685503edae12
Jeff F <jeff@jrfink.net>
2023-10-16 22:56:33 -0400

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -90,14 +90,13 @@ const indexPage = () => {
 <p>
    On a scale of 1 - 5, how are you enjoying the site? 
 <br />
-
+<br />
 <div className={style.flexContainer}>
 <br />
 <br />
 <button>1</button> &nbsp; &nbsp; <button>2</button> &nbsp;  &nbsp; <button>3</button> &nbsp; &nbsp; <button>4</button> &nbsp; &nbsp; <button>5</button>
 </div>
 </p>
-<br /> 
 <br />
                 <h3> 
                     If you&#39;re in college&#46;&#46;&#46; 
