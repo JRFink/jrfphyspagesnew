f31ef1c1bd356595b290cd6850028301c4506338
Jeff Fink <fink.jeffr@gmail.com>
2023-10-20 22:39:23 -0400

    Update index.tsx

--- pages/research/qis/index.tsx
+++ pages/research/qis/index.tsx
@@ -127,7 +127,7 @@ const QuantI = () => {
 <br />
                         <li><Link href="https://ionq.com/">Ion Q</Link></li>
 <ul>
-     <li>NYSE: </li>
+     <li>NYSE: IONQ &#36;12.59</li>
      <li>Q1 revenue &#36;4.3 million</li>
      <li>Q2 revenue &#36;5.5 million</li>
      <li>Initially raised</li>
