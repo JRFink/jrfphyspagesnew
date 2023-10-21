829222341dbba9448a6cda8daaa995f8b8052a7d
Jeff F <jeff@jrfink.net>
2023-10-21 09:42:42 -0400

    ff

--- pages/research/qis/index.tsx
+++ pages/research/qis/index.tsx
@@ -145,6 +145,8 @@ const QuantI = () => {
                         <br />
                         <li><Link href="https://alice-bob.com/">Alice and Bob</Link></li>
                         <br />
+                        <li><Link href="https://www.quantum-x.io/">Quantum - X</Link></li>
+                        <br />
                     </ul>
                     <br />
                     
