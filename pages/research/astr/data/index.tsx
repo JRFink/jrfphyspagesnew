a0221ef20d3e5358af832b4fafd36578ca19d13b
Jeff F <jeff@jrfink.net>
2023-10-23 15:36:21 -0400

    ff

--- pages/research/astr/data/index.tsx
+++ pages/research/astr/data/index.tsx
@@ -1,23 +1,36 @@
+import {useEffect} from 'react';
+import Script from 'next/script';
 import Link from 'next/link';  
  import Image from 'next/image'; 
  import style from '@/styles/general.module.css'; 
   
  const Equat = () => { 
-     return ( 
-         <> 
-             <br /> 
-             <Link href="/research/astr" className={style.backtoLink}>Astrophysics</Link> 
-             <br /> 
-             <br /> 
-             <h1 className={style.centerText}> 
+
+    useEffect(() => {
+        if (typeof window?.MathJax !== "undefined") {
+            window.MathJax.typeset()
+        }
+    },[])
+
+    return ( 
+        <> 
+            <Script 
+                src="https://cdn.jsdelivr.net/npm/mathjax@3.0.0/es5/tex-chtml.js"
+                strategy="lazyOnload"
+            />
+            <br /> 
+            <Link href="/research/astr" className={style.backtoLink}>Astrophysics</Link> 
+            <br /> 
+            <br /> 
+            <h1 className={style.centerText}> 
                 Data
-             </h1> 
-             <p> 
-                Data for astrophysics research...  
-             </p> 
-  
-         </> 
-     ); 
- } 
-  
- export default Equat;
+            </h1> 
+            <p> 
+                The cosmological constant is about {"\\( \\approx \\ 10^{-35} \\ s^{-2}  \\)"} 
+            </p> 
+
+        </> 
+    ); 
+} 
+
+export default Equat;
