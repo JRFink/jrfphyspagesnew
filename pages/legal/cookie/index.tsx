a8a0158e4c7fe1a9673d3d41d8021db504adf2e7
Jeff F <jeff@jrfink.net>
2023-10-23 22:44:09 -0400

    ff

--- pages/legal/cookie/index.tsx
+++ pages/legal/cookie/index.tsx
@@ -6,9 +6,9 @@ const Cookie = () => {
             <h1 className={style.centerText}>
                 Cookie Policy
             </h1>
-            <p>
-                We use cookies to make the site function properly. If you do not want to be subject to any cookies, please leave the site now. 
-            </p>
+                <p>
+                    We use cookies to make the site function properly. If you do not want to be subject to any cookies, please leave the site now. 
+                </p>
         </>
     );
 }
