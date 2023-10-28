3aba124193cf8871656ee14644a0680d1ea68aa4
Jeff Fink <fink.jeffr@gmail.com>
2023-10-27 22:05:55 -0400

    Update index.tsx

--- pages/education/em1/index.tsx
+++ pages/education/em1/index.tsx
@@ -15,6 +15,8 @@ const Em1 = () => {
                 <Link href="/education/em1/quiz" className={style.styleCourseNav}>Quizzes</Link> 
                 <Link href="/education/em1/hw" className={style.styleCourseNav}>Homework</Link>  
                 <Link href="/education/em1/equat" className={style.styleCourseNav}>Equations</Link>
+
+               <Link href="/education/em1/tbook" className={style.styleCourseNav}>Textbooks</Link>
                 <Link href="/education/em1/stuff" className={style.styleCourseNav}>Question database</Link>
                 <Link href="/education/em1/prob" className={style.styleCourseNav}>Real world problems</Link>
                 {/* <Link href="/education/em1/expver" className={style.styleCourseNav}>Experimental verification</Link> */}
