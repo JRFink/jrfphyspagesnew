59a14d733cbf8935092b6390753564dd94dbedeb
Jeff F <jeff@jrfink.net>
2023-10-31 01:58:58 -0400

    ff

--- pages/education/qm1/qdatabase/griffiths/index.tsx
+++ pages/education/qm1/qdatabase/griffiths/index.tsx
@@ -40,14 +40,20 @@ const Griffith = () => {
                 Problem 1.5
                 <p>
                     Consider the wave function 
-                    {"\\( \\Psi(x, t) = A e^{- \\lambda \\vert x \\vert } e^{-i \\omega t} \\)"}, 
+                    {"\\( \\Psi(x, t) = A e^{- \\lambda \\vert x \\vert } \\; e^{-i \\omega t} \\)"}, 
                     <br />
-                    where <em>A, &#955;,</em> and <em> &#969;</em> are positive real constants. &#40;We&#39;ll see in 
+                    where {"\\( A, \\lambda \\)"} and {"\\( \\omega \\)"} are positive real constants. &#40;We&#39;ll see in 
                     Chapter 2 for what potential <em>V</em> this wave function satisfies the Schrodinger equation.&#41;
                     <br />
                     <br />
                     <b>&#40;a&#41;</b> Normalize {"\\( \\Psi \\)"}. 
                     <br />
+                    <b>&#40;b&#41;</b> Determine the expectation value of <em>x</em> and <em>x<span><sup>2</sup></span></em>. 
+                    <br />
+                    <b>&#40;c&#41;</b> Find the standard deviation of x. Sketch the graph of {"\\( \\vert \\Psi \\vert^{2} \\)"},
+                    as a function of <em>x</em>, and mark the points {"\\( ( \\langle x \\rangle + \\sigma ) \\)"} 
+                    and {"\\( ( \\langle x \\rangle - \\sigma ) \\)"}, to illustrate the sense in which {" \\( \\sigma \\)"}
+                    represents the "spread" in <em>x</em>. What is the probability that the particle would be found outside this range?
                 </p>
             </p>
             
