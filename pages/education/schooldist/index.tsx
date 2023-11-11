a031ce62744de2c38157b539521f0eebf342e239
Jeff Fink <jeff@jrfink.net>
2023-11-11 02:02:34 -0500

    Create index.tsx

---
+++ pages/education/schooldist/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'; 
+import style from '@/styles/general.module.css'; 
+
+const SchoolD = () => { 
+    return ( 
+       <>
+          <h1 className={style.centerText}>
+              School districts 
+          </h1>
+       </>
+    );
+}
+
+export default SchoolD;
