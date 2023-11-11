d21d96302836227f0598f4722d4f9ed127cc22dd
Jeff Fink <fink.jeffr@gmail.com>
2023-11-11 01:59:07 -0500

    Update footer.tsx

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -48,6 +48,7 @@ const Footer = () => {
                     </div>
                     <div>
                         <small>
+                            <Link href="/education/schooldist" className={style.linkFooter}>School districts</Link><br />
                             <Link href="/research/financial" className={style.linkFooter}>Financials of research</Link><br></br>
                             <br /> 
                             <Link href="/legal/intern" className={style.linkFooter}>International fun</Link><br></br>
