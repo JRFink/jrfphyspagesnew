9070876d24b742230d885ef34ea6845ed43e0a1f
Jeff Fink <fink.jeffr@gmail.com>
2023-11-13 22:32:59 -0500

    Update index.tsx

--- pages/education/gp1/wke/index.tsx
+++ pages/education/gp1/wke/index.tsx
@@ -24,7 +24,7 @@ const Wke = () => {
             <WkeVid />
             <br></br>
             <p>
-                In trying to solve physical problems, sometimes we can apply the strategy we used in the previous sections; that is, articulate newton&#39;s law for a situation, amd if the differential equation is easy to solve, then solve it. However, sometimes the differential equation is not easy to solve, and that is when you result to other means to solve physical problems. 
+                In trying to solve physical problems, sometimes we can apply the strategy we used in the previous sections; that is, articulate newton&#39;s law for a situation, and if the differential equation is easy to solve, then solve it. However, sometimes the differential equation is not easy to solve, and that is when you result to other means to solve the problem. 
             </p>
             <p> 
                 For example...
