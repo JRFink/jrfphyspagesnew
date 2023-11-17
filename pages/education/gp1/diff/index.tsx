43a977719458f2dbb194804e01d4efa76d585eba
Jeff Fink <fink.jeffr@gmail.com>
2023-11-17 18:48:39 -0500

    Update index.tsx

--- pages/education/gp1/diff/index.tsx
+++ pages/education/gp1/diff/index.tsx
@@ -27,7 +27,7 @@ const Osc = () => {
                 As a prelude to describing what causes changes in motion, we study differential equations. These equations help us understand how to link the changes in motion to what caused them.
             </p> 
             <p> 
-                Differential equations isn&#39;t something you typically learn in gen phys 1. Learning a few pieces of it, though, will drastically help you understand Newton&#39;s second law more effectively. They will help you understand how to solve the second law in different cases. In some cases it is easy to solve, but in other cases it isn&#39;t, and that&#39;s when you have to result to other strategies to solve physical problems.
+                Differential equations isn&#39;t something you typically learn in gen phys 1. Learning a few pieces of it, though, will drastically help you understand Newton&#39;s second law more effectively. They will help you understand how to solve the second law in different cases. In some cases the equation is easy to solve, but in other cases it is not, and that is when you have to result to other strategies to solve physical problems.
             </p> 
             <br />
             <h2 className={style.h2SectionColor} id="types">
