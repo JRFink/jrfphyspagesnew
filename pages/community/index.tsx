278ddc03d4526c61c17f5ee1e6da37a334057c81
Jeff Fink <fink.jeffr@gmail.com>
2023-11-18 00:21:13 -0500

    Update index.tsx

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -11,11 +11,8 @@ const Comm = () => {
                 Hello, and welcome to the physics community! 
             </p>
             <p>
-                This page is all about connecting with others in physics. For example, if you&#39;re taking a physics class here and need help, you can post a question here. 
-            <br /> 
+                This page is all about connecting with others in physics. For example, if you&#39;re taking a physics class here and need help, you can post a question. 
             <br />
-            If you work in a specific area of physics and want to connect with others in that area, you can find them here. 
-            <br /> 
             <br /> 
             Or if you just want to post funny memes, you can do that too. 
                 </p>
