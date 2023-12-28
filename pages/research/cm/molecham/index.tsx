a4c5a544adb14b3bcc2374b74d111c902a8776d2
Jeff F <jeff@jrfink.net>
2023-12-28 10:29:08 -0500

    s

--- pages/research/cm/molecham/index.tsx
+++ pages/research/cm/molecham/index.tsx
@@ -1,10 +1,22 @@
 import Link from 'next/link'; 
-import Image from 'next/image';
+import Script from 'next/script';
+import {useEffect} from 'react';
 import style from '@/styles/general.module.css';
 
 const MolecHam = () => {
+
+    useEffect(() => {
+        if (typeof window?.MathJax !== "undefined") {
+            window.MathJax.typeset()
+        }
+    },[])
+
     return (
         <>
+            <Script 
+                src="https://cdn.jsdelivr.net/npm/mathjax@3.0.0/es5/tex-chtml.js"
+                strategy="lazyOnload"
+            />
             <br />
             <Link href="/research/cm" className={style.backtoLink}>Condensed Matter</Link>
             <br />
@@ -13,7 +25,9 @@ const MolecHam = () => {
                 Molecular Hamiltonians
             </h1>
             <br />
-           
+            <div className={style.equationBox}>
+                <div className={style.equation}>{"\\( \\)"} &nbsp; &nbsp; &nbsp; &nbsp; {"( )"}</div>
+            </div>
         </>
     );
 }
