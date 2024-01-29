55a3360599d1a9f854d4f9b230481d14d592ca08
Jeff Fink <jeff@jrfink.net>
2024-01-29 09:09:14 -0500

    Update index.tsx

--- pages/education/hop2/index.tsx
+++ pages/education/hop2/index.tsx
@@ -6,7 +6,7 @@ const Hop2 = () => {
     return (
         <>
             <br />
-            <Link href="/education/topics" className={style.backtoLink}>Topics</Link><br></br>
+            <Link href="/education" className={style.backtoLink}>Education</Link><br></br>
             <br></br>
             <h1 className={style.centerText}>
                 History of Physics 2
