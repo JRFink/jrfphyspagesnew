870de0fd57d0dec990f8a28098d142b63696bdc2
Jeff Fink <jeff@jrfink.net>
2024-01-29 09:10:24 -0500

    Update index.tsx

--- pages/education/qm2/index.tsx
+++ pages/education/qm2/index.tsx
@@ -6,7 +6,7 @@ const Qm2 = () => {
     return (
         <>
             <br />
-            <Link href="/education/topics" className={style.backtoLink}>Topics</Link><br></br>
+            <Link href="/education" className={style.backtoLink}>Education</Link><br></br>
             <br></br>
             <h1 className={style.centerText}>
                 Quantum Mechanics 2
